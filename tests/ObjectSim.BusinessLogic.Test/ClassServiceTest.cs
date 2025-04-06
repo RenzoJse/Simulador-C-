a3e7191432959e0720323d3df9d11eaeaaa9110d
RenzoJse <renzojose007@gmail.com>
2025-04-06 19:56:09 -0300

    CreateClass_WithoutName_ThrowsException RED

--- tests/ObjectSim.BusinessLogic.Test/ClassServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/ClassServiceTest.cs
@@ -1,5 +1,19 @@
-﻿namespace ObjectSim.Domain.Test;
+namespace ObjectSim.BusinessLogic.Test;
 
+[TestClass]
 public class ClassServiceTest
 {
+    #region Create
+    #region Error
+
+    [TestMethod]
+    public void CreateClass_WithoutName_ThrowsException()
+    {
+        var args = new CreateClassArgs(Name: null, IsAbstract: "Description", IsSealed: true, Attribute: new List<Attributes>(), Methods: new List<Method>(), Parent: Class, Id: Guid.NewGuid());
+
+        _classService.CreateClass(args);
+    }
+
+    #endregion
+    #endregion
 }
