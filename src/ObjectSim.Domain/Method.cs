ef9daece0b887875db8460e8c6216ae76f84995c
Matias Fontes <matifontes13@gmail.com>
2025-04-14 16:32:39 -0300

    refactor: method class

--- src/ObjectSim.Domain/Method.cs
+++ src/ObjectSim.Domain/Method.cs
@@ -2,7 +2,7 @@
 
 public class Method
 {
-    public int Id { get; set; };
+    public int Id { get; set; }
     public string Name { get; set; } = null;
     public string Type { get; set; } = null;
     public bool Abstract { get; set; } = false;
