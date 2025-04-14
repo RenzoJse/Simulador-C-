3ebb7b721931aa1af05de0a8689c974357560f95
Matias Fontes <matifontes13@gmail.com>
2025-04-14 15:52:37 -0300

    code: implemented create valid method test [RED]

--- tests/ObjectSim.BusinessLogic.Test/MethodServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/MethodServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using ObjectSim.IDataAccess;
 using Moq;
 using ObjectSim.Domain;
+using System.Linq.Expressions;
+using FluentAssertions;
 
 namespace ObjectSim.BusinessLogic.Test;
 
@@ -15,5 +17,29 @@ public class MethodServiceTest
     {
         _methodRepositoryMock = new Mock<IMethodRepository<Method>>(MockBehavior.Strict);
         _methodService = new MethodService(_methodRepositoryMock.Object);
+    }
+
+
+    [TestMethod]
+    public void CreateValidMethod()
+    {
+        var testMethod = new Method
+        {
+            Name = "MetodoDePrueba",
+            Type = "boolean",
+            Abstract = false,
+            IsSealed = false,
+            Accessibility = "public",
+            Parameters = new List<Parameter>(),
+            LocalVariables = new List<LocalVariable>()
+        };
+
+        _methodRepositoryMock.Setup(repo => repo.Exist(It.IsAny<Expression<Func<Method, bool>>>())).Returns(false);
+        _methodRepositoryMock.Setup(repo => repo.Add(It.IsAny<Method>())).Returns((Method act) => act);
+
+        var result = _methodService.Create(testMethod);
+
+        result.Should().NotBeNull();
+        _methodRepositoryMock.VerifyAll();
     }
 }
