f591c61303498f683de7dc8ed18ef263317c6f50
Matias Fontes <matifontes13@gmail.com>
2025-04-14 15:35:36 -0300

    code: implemented basic method service test class [RED]

--- tests/ObjectSim.BusinessLogic.Test/MethodServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/MethodServiceTest.cs
@@ -1,4 +1,20 @@
-﻿namespace ObjectSim.BusinessLogic.Test;
-internal class MethodServiceTest
+﻿using ObjectSim.IDataAccess;
+using Moq;
+using ObjectSim.Domain;
+using ObjectSim.BusinessLogic;
+
+namespace ObjectSim.BusinessLogic.Test;
+
+[TestClass]
+public class MethodServiceTest
 {
+    private Mock<IMethopdRepository<Method>>? _methodRepositoryMock;
+    private MethodService?  _methodService;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _methodRepositoryMock = new Mock<IMethopdRepository<Method>>(MockBehavior.Strict);
+        _methodService = new MethodService(_methodRepositoryMock.Object);
+    }
 }
