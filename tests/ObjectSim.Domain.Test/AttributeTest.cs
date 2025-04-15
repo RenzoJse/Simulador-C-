d606807436201b80e894ce64c48b561246fdafc5
joaquincalvo <joaquincalvo7@gmail.com>
2025-04-15 19:23:10 -0300

    code:red validateAttributeTest

--- tests/ObjectSim.Domain.Test/AttributeTest.cs
+++ tests/ObjectSim.Domain.Test/AttributeTest.cs
@@ -3,24 +3,37 @@
 public class AttributeTest
 {
     [TestMethod]
-    public void AttributeDataTypeTest()
+    public void AttributeDataTypeCreateAttribute_OKTest()
     {
         var attribute = new Attribute();
         attribute.DataType = Attribute.AttributeDataType.String;
         Assert.AreEqual(Attribute.AttributeDataType.String, attribute.DataType);
     }
     [TestMethod]
-    public void AttributeVisibilityTest()
+    public void AttributeVisibilityCreateAttribute_OKTest()
     {
         var attribute = new Attribute();
         attribute.Visibility = Attribute.AttributeVisibility.Public;
         Assert.AreEqual(Attribute.AttributeVisibility.Public, attribute.Visibility);
     }
     [TestMethod]
-    public void AttributeNameTest()
+    public void AttributeName_CreateAttribute_OKTest()
     {
         var attribute = new Attribute();
         attribute.Name = "TestAttribute";
         Assert.AreEqual("TestAttribute", attribute.Name);
+    }
+    [TestMethod]
+    public void Validate_ValidAttribute_ShouldNotThrow()
+    {
+        var attribute = new Attribute
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test",
+            DataType = Attribute.AttributeDataType.String,
+            Visibility = Attribute.AttributeVisibility.Public
+        };
+
+        AttributeValidator.Validate(attribute);
     }
 }
