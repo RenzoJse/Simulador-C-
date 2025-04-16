f041211fbc0ab8538da6de6fe7ef8406e10eb4cc
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:30:49 -0300

    code: implemented basic PArameterService [GREEN]

---
+++ src/ObjectSim.BusinessLogic/ParameterService.cs
@@ -0,0 +1,12 @@
+﻿using ObjectSim.Domain;
+using ObjectSim.IBusinessLogic;
+using ObjectSim.IDataAccess;
+
+namespace ObjectSim.BusinessLogic;
+public class ParameterService(IParameterRepository<Parameter> parameterRepository) : IParameterService<Parameter>
+{
+    public Parameter Create(Parameter Entity)
+    {
+        throw new NotImplementedException();
+    }
+}
