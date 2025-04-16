39345cb44bacbe9c6aaa89deddf55e39f5ec9035
joaquincalvo <joaquincalvo7@gmail.com>
2025-04-15 23:00:46 -0300

    code:green ValidateAttributeNull

--- src/ObjectSim.Domain/Attribute.cs
+++ src/ObjectSim.Domain/Attribute.cs
@@ -27,12 +27,25 @@ public class Attribute
     public void Validate()
     {
         ValidateId(Id);
+        ValidateName(Name);
     }
     private static void ValidateId(Guid id)
     {
         if(id == Guid.Empty)
         {
             throw new ArgumentException("Id must be a valid non-empty GUID.");
+        }
+    }
+    private static void ValidateName(string? name)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be null or whitespace.");
+        }
+
+        if(name.Length > 100)
+        {
+            throw new ArgumentException("Name cannot exceed 100 characters.");
         }
     }
 }
