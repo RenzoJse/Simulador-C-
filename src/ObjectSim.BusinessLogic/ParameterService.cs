ba322db6c3e3daa95e5e0dab132c23541a1b3c85
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:40:40 -0300

    code: implemented create parameter service [GREEN]

--- src/ObjectSim.BusinessLogic/ParameterService.cs
+++ src/ObjectSim.BusinessLogic/ParameterService.cs
@@ -7,6 +7,25 @@ public class ParameterService(IParameterRepository<Parameter> parameterRepositor
 {
     public Parameter Create(Parameter Entity)
     {
-        throw new NotImplementedException();
+        if(Entity == null)
+        {
+            throw new Exception("Parameter cannot be null");
+        }
+
+        var existParameter = parameterRepository.Exist(m => m.Name == Entity.Name);
+        if(existParameter)
+        {
+            throw new Exception("Parameter already exist");
+        }
+
+        var parameterToAdd = new Parameter
+        {
+            Id = Entity.Id,
+            Name = Entity.Name,
+            Type = Entity.Type,
+        };
+
+        parameterRepository.Add(parameterToAdd);
+        return parameterToAdd;
     }
 }
