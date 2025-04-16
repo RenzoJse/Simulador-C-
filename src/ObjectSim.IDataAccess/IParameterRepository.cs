7498e04ded47a400b52e14989fd229a9e66b03fd
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:36:56 -0300

    code: implemented valid and invalid test methods to Create  Parameters

--- src/ObjectSim.IDataAccess/IParameterRepository.cs
+++ src/ObjectSim.IDataAccess/IParameterRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace ObjectSim.IDataAccess;
+﻿using System.Linq.Expressions;
+
+namespace ObjectSim.IDataAccess;
 public interface IParameterRepository<TEntity>
        where TEntity : class
 {
     TEntity Add(TEntity entity);
+    bool Exist(Expression<Func<TEntity, bool>> expression);
 }
--- tests/ObjectSim.BusinessLogic.Test/ParameterServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/ParameterServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using System.Linq.Expressions;
+using FluentAssertions;
+using Moq;
 using ObjectSim.Domain;
 using ObjectSim.IBusinessLogic;
 using ObjectSim.IDataAccess;
@@ -18,12 +20,31 @@ public class ParameterServiceTest
     public void Setup()
     {
         _parameterRepositoryMock = new Mock<IParameterRepository<Parameter>>(MockBehavior.Strict);
-        _parameterService = new MethodService(_parameterRepositoryMock.Object);
+        _parameterService = new ParameterService(_parameterRepositoryMock.Object);
         parameter = new Parameter
         {
             Id = ClassId,
             Name = "Parameter1",
             Type = "string"
         };
+    }
+
+    [TestMethod]
+    public void CreateValidParameter()
+    {
+
+        _parameterRepositoryMock!.Setup(repo => repo.Exist(It.IsAny<Expression<Func<Parameter, bool>>>())).Returns(false);
+        _parameterRepositoryMock.Setup(repo => repo.Add(It.IsAny<Parameter>())).Returns((Parameter act) => act);
+
+        var result = _parameterService!.Create(parameter!);
+
+        result.Should().NotBeNull();
+        _parameterRepositoryMock.VerifyAll();
+    }
+
+    [TestMethod]
+    public void CreateNullParameter()
+    {
+        _parameterService!.Create(null!);
     }
 }
