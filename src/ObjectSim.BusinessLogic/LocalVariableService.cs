5f1cbd3f2e89f6475df2082a06e13e4310af9eb1
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:52:57 -0300

    code: implemented basic localvariable service [GREEN]

---
+++ src/ObjectSim.BusinessLogic/LocalVariableService.cs
@@ -0,0 +1,12 @@
+﻿using ObjectSim.Domain;
+using ObjectSim.IBusinessLogic;
+using ObjectSim.IDataAccess;
+
+namespace ObjectSim.BusinessLogic;
+public class LocalVariableService(ILocalVariableRepository<LocalVariable> localVariableRepostiroy) : ILocalVariableService<LocalVariable>
+{
+    public LocalVariable Create(LocalVariable Entity)
+    {
+        throw new NotImplementedException();
+    }
+}
