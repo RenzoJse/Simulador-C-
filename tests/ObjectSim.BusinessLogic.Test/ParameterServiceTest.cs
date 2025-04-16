6244eca38f3ad56216975b951df60db1b0b771ed
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:28:29 -0300

    code: implemented ParameterServiceTest class and test initialize [RED]

--- tests/ObjectSim.BusinessLogic.Test/ParameterServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/ParameterServiceTest.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using ObjectSim.Domain;
+using ObjectSim.IBusinessLogic;
+using ObjectSim.IDataAccess;
 
 namespace ObjectSim.BusinessLogic.Test;
-internal class ParameterServiceTest
+
+[TestClass]
+public class ParameterServiceTest
 {
+    private Mock<IParameterRepository<Parameter>>? _parameterRepositoryMock;
+    private ParameterService? _parameterService;
+    private static readonly Guid ClassId = Guid.NewGuid();
+    private Parameter? parameter;
+
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _parameterRepositoryMock = new Mock<IParameterRepository<Parameter>>(MockBehavior.Strict);
+        _parameterService = new MethodService(_parameterRepositoryMock.Object);
+        parameter = new Parameter
+        {
+            Id = ClassId,
+            Name = "Parameter1",
+            Type = "string"
+        };
+    }
 }
