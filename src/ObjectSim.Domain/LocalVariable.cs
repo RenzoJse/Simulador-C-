20eb59c4ad47904d1d00d6884d82d1b9f2484807
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:52:38 -0300

    code: implemented guid id for local variable

--- src/ObjectSim.Domain/LocalVariable.cs
+++ src/ObjectSim.Domain/LocalVariable.cs
@@ -1,6 +1,7 @@
 ﻿namespace ObjectSim.Domain;
 public class LocalVariable
 {
+    public Guid? Id { get; set; }
     public string? Name { get; set; } = null;
     public string? Type { get; set; } = null;
 }
