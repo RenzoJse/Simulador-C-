6ac5abaadd4b71c92f2b65faecda25877bb56907
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:50:18 -0300

    code: implemented Local variable test class and setup [RED]

---
+++ tests/ObjectSim.BusinessLogic.Test/LocalVariableServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using ObjectSim.Domain;
+using ObjectSim.IDataAccess;
+
+namespace ObjectSim.BusinessLogic.Test;
+[TestClass]
+public class ParameterServiceTest
+{
+    private Mock<ILocalVariableRepository<LocalVariable>>? _localVariableRepositoryMock;
+    private LocalVariableService? _localVariableService;
+    private static readonly Guid ClassId = Guid.NewGuid();
+    private LocalVariable? _localVariable;
+
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _localVariableRepositoryMock = new Mock<ILocalVariableRepository<LocalVariable>>(MockBehavior.Strict);
+        _localVariableService = new LocalVariableService(_localVariableRepositoryMock.Object);
+        _localVariable = new LocalVariable
+        {
+            Id = ClassId,
+            Name = "Parameter1",
+            Type = "string"
+        };
+    }
+}
