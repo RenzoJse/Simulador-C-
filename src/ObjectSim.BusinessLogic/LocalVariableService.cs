656b1286cc92c076df2d7314d32962a743a2baab
Matias Fontes <matifontes13@gmail.com>
2025-04-15 23:59:29 -0300

    code: implemented localvariable create service [GREEN]

--- src/ObjectSim.BusinessLogic/LocalVariableService.cs
+++ src/ObjectSim.BusinessLogic/LocalVariableService.cs
@@ -7,6 +7,25 @@ public class LocalVariableService(ILocalVariableRepository<LocalVariable> localV
 {
     public LocalVariable Create(LocalVariable Entity)
     {
-        throw new NotImplementedException();
+        if(Entity == null)
+        {
+            throw new Exception("LocalVariable cannot be null");
+        }
+
+        var existLocalVariable = localVariableRepostiroy.Exist(m => m.Name == Entity.Name);
+        if(existLocalVariable)
+        {
+            throw new Exception("LocalVariable already exist");
+        }
+
+        var localVariableToAdd = new LocalVariable
+        {
+            Id = Entity.Id,
+            Name = Entity.Name,
+            Type = Entity.Type,
+        };
+
+        localVariableRepostiroy.Add(localVariableToAdd);
+        return localVariableToAdd;
     }
 }
