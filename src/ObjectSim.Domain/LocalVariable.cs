9535a9bf0d3df77623b1c2eddb7ba11c40966091
RenzoJse <renzojose007@gmail.com>
2025-04-16 13:40:02 -0300

    Code: Added IDs

--- src/ObjectSim.Domain/LocalVariable.cs
+++ src/ObjectSim.Domain/LocalVariable.cs
@@ -1,6 +1,7 @@
 ﻿namespace ObjectSim.Domain;
 public class LocalVariable
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string? Name { get; set; } = null;
     public string? Type { get; set; } = null;
 }
--- src/ObjectSim.Domain/Parameter.cs
+++ src/ObjectSim.Domain/Parameter.cs
@@ -1,6 +1,7 @@
 ﻿namespace ObjectSim.Domain;
 public class Parameter
 {
+    public Guid Id { get; init; } = Guid.NewGuid();
     public string? Name { get; set; } = null;
     public string? Type { get; set; } = null;
 }
