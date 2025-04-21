76e08e6cbf822b346697a2a2b61a8feebe8d368a
Matias Fontes <matifontes13@gmail.com>
2025-04-21 14:48:42 -0300

    code: implemented Method DataType SetAndGet test [RED]

--- tests/ObjectSim.Domain.Test/MethodTest.cs
+++ tests/ObjectSim.Domain.Test/MethodTest.cs
@@ -1,10 +1,25 @@
+using FluentAssertions;
+
 namespace ObjectSim.Domain.Test;
 
 [TestClass]
 public class MethodTest
 {
     [TestMethod]
-    public void CreateValidMethod()
+    public void DataType_Property_SetAndGet_ShouldBeEqual()
     {
+        var method = new Method();
+        method.DataType = Method.MethodDataType.Decimal;
+        method.DataType.Should().Be(Method.MethodDataType.Decimal);
+    }
+
+    [TestMethod]
+    public void Visibility_Property_SetAndGet_ShouldBeEqual()
+    {
+        var method = new Method();
+
+        method.Visibility = Method.MethodVisibility.ProtectedInternal;
+
+        method.Visibility.Should().Be(Method.MethodVisibility.ProtectedInternal);
     }
 }
