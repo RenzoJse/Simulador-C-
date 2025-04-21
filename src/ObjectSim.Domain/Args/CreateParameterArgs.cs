72ca14d45181b71e19dc62c3801dd76c49f146d3
Matias Fontes <matifontes13@gmail.com>
2025-04-21 16:09:23 -0300

    code: implemented Create method and create parameter args

---
+++ src/ObjectSim.Domain/Args/CreateParameterArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectSim.Domain.Args;
+public class CreateParameterArgs(
+    string name,
+    string type)
+{
+    public string? Name { get; set; } = name;
+    public string Type { get; set; } = type;
+}
