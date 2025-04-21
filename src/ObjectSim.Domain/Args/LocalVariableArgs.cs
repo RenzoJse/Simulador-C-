0fbbdf23a63bb04adf8e63e314740092c07589fb
Matias Fontes <matifontes13@gmail.com>
2025-04-21 16:09:11 -0300

    code: create LocalVariable Arg

---
+++ src/ObjectSim.Domain/Args/LocalVariableArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectSim.Domain.Args;
+public class CreateLocalVariableArgs(
+    string name,
+    string type)
+{
+    public string? Name { get; set; } = name;
+    public string Type { get; set; } = type;
+}
