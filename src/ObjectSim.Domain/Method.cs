3befee7eb634c5beaa98b4086b893e273e6638c9
Matias Fontes <matifontes13@gmail.com>
2025-04-21 14:55:06 -0300

    code: implemented Method Data Type and Method Accessibility [GREEN]

--- src/ObjectSim.Domain/Method.cs
+++ src/ObjectSim.Domain/Method.cs
@@ -2,12 +2,30 @@
 
 public class Method
 {
+    public enum MethodDataType
+    {
+        String,
+        Char,
+        Int,
+        Decimal,
+        Bool,
+        DateTime
+    }
+    public enum MethodAccessibility
+    {
+        Public,
+        Private,
+        Protected,
+        Internal,
+        ProtectedInternal,
+        PrivateProtected
+    }
     public Guid Id { get; set; }
     public string? Name { get; set; } = null;
-    public string? Type { get; set; } = null;
+    public MethodDataType? Type { get; set; } = null;
     public bool Abstract { get; set; } = false;
     public bool IsSealed { get; set; } = false;
-    public string? Accessibility { get; set; } = null;
+    public MethodAccessibility? Accessibility { get; set; } = null;
     public List<Parameter> Parameters { get; set; } = [];
     public List<LocalVariable> LocalVariables { get; set; } = [];
 }
