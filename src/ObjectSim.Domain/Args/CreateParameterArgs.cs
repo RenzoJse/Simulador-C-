e76f80f6d53b4d4caf806189f6dfcf2b8ae30f6c
Matias Fontes <matifontes13@gmail.com>
2025-04-21 19:47:28 -0300

    delete invalid args

--- src/ObjectSim.Domain/Args/CreateParameterArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ObjectSim.Domain.Args;
-public class CreateParameterArgs(
-    string name,
-    string type)
-{
-    public string? Name { get; set; } = name;
-    public string Type { get; set; } = type;
-}
--- src/ObjectSim.Domain/Args/LocalVariableArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ObjectSim.Domain.Args;
-public class CreateLocalVariableArgs(
-    string name,
-    string type)
-{
-    public string? Name { get; set; } = name;
-    public string Type { get; set; } = type;
-}
