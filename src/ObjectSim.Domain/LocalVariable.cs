84603ee98a1b8fdcf9b67247d0662502d129abfa
Matias Fontes <matifontes13@gmail.com>
2025-04-23 16:00:09 -0300

    code: implemented validations for LocalVariable [GREEN]

--- src/ObjectSim.Domain/LocalVariable.cs
+++ src/ObjectSim.Domain/LocalVariable.cs
@@ -1,7 +1,66 @@
-﻿namespace ObjectSim.Domain;
+﻿using System.Text.RegularExpressions;
+
+namespace ObjectSim.Domain;
 public class LocalVariable
 {
+    public enum LocalVariableDataType
+    {
+        String,
+        Char,
+        Int,
+        Decimal,
+        Bool,
+        DateTime
+    }
     public Guid? Id { get; set; }
-    public string? Name { get; set; } = null;
-    public string? Type { get; set; } = null;
+
+    #region Name
+    private string? _name;
+    public string? Name
+    {
+        get => _name;
+        set
+        {
+            ValidateName(value!);
+            _name = value;
+        }
+    }
+
+    private static void ValidateName(string? name)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be null or whitespace.");
+        }
+
+        if(name.Length > 100)
+        {
+            throw new ArgumentException("Name cannot exceed 100 characters.");
+        }
+
+        if(Regex.IsMatch(name, @"^\d"))
+        {
+            throw new ArgumentException("Name cannot be null or start with a num.");
+        }
+    }
+    #endregion
+    #region Type
+    private LocalVariableDataType _type;
+    public LocalVariableDataType Type
+    {
+        get => _type;
+        set
+        {
+            ValidateDataType(value!);
+            _type = value;
+        }
+    }
+    private static void ValidateDataType(LocalVariableDataType type)
+    {
+        if(!Enum.IsDefined(typeof(LocalVariableDataType), type))
+        {
+            throw new ArgumentException("Invalid data type.");
+        }
+    }
+    #endregion
 }
