00f507790853ec7399f897bfa15a1fb2a9e17504
RenzoJse <renzojose007@gmail.com>
2025-04-24 11:29:29 -0300

    Code: Added missing CreateClass Tests

--- src/ObjectSim.BusinessLogic/ClassLogic/ClassService.cs
+++ src/ObjectSim.BusinessLogic/ClassLogic/ClassService.cs
@@ -1,5 +1,6 @@
 ﻿using ObjectSim.BusinessLogic.ClassLogic.ClassBuilders;
 using ObjectSim.BusinessLogic.ClassLogic.Strategy;
+using ObjectSim.DataAccess.Interface;
 using ObjectSim.Domain;
 using ObjectSim.Domain.Args;
 using ObjectSim.IBusinessLogic;
@@ -7,15 +8,18 @@ using Attribute = ObjectSim.Domain.Attribute;
 
 namespace ObjectSim.BusinessLogic.ClassLogic;
 
-public class ClassService(List<IBuilderStrategy> strategies) : IClassService
+public class ClassService(List<IBuilderStrategy> strategies, IRepository<Class> classRepository) : IClassService
 {
     public Class CreateClass(CreateClassArgs args)
     {
+        ArgumentNullException.ThrowIfNull(args);
+
         var builder = GetBuilder(args);
         builder.SetParent(args.Parent);
         builder.SetName(args.Name!);
         builder.SetAbstraction(args.IsAbstract);
         builder.SetInterface(args.IsInterface);
+        builder.SetSealed(args.IsSealed);
         builder.SetAttributes(args.Attributes);
         builder.SetMethods(args.Methods);
         return builder.GetResult()!;
@@ -30,7 +34,11 @@ public class ClassService(List<IBuilderStrategy> strategies) : IClassService
 
     public Class GetById(Guid? classId)
     {
-        throw new NotImplementedException();
+        if (classId == null)
+        {
+            throw new ArgumentNullException(nameof(classId));
+        }
+        return classRepository.Get(c => c.Id == classId) ?? throw new ArgumentException("Class not found.");
     }
 
     public void AddMethod(Guid classId, Method method)
--- tests/ObjectSim.BusinessLogic.Test/ClassLogic/ClassServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/ClassLogic/ClassServiceTest.cs
@@ -4,6 +4,7 @@ using ObjectSim.BusinessLogic.ClassLogic;
 using ObjectSim.BusinessLogic.ClassLogic.ClassBuilders;
 using ObjectSim.BusinessLogic.ClassLogic.ClassBuilders.Builders;
 using ObjectSim.BusinessLogic.ClassLogic.Strategy;
+using ObjectSim.DataAccess.Interface;
 using ObjectSim.Domain;
 using ObjectSim.Domain.Args;
 using ObjectSim.IBusinessLogic;
@@ -17,7 +18,9 @@ public class ClassServiceTest
     private ClassService? _classServiceTest;
     private Mock<IBuilderStrategy>? _builderStrategyMock;
     private Mock<IAttributeService>? _attributeServiceMock;
+    private Mock<IClassService>? _classServiceMock;
     private Mock<IMethodService>? _methodServiceMock;
+    private Mock<IRepository<Class>>? _classRepositoryMock;
 
     //private static readonly Builder ClassBuilder = new ClassBuilder(null!, null!, null!);
 
@@ -31,16 +34,15 @@ public class ClassServiceTest
 
     protected virtual Builder GetMockedBuilder()
     {
-        var methodServiceMock = new Mock<IMethodService>();
-        var classServiceMock = new Mock<IClassService>();
-        var attributeServiceMock = new Mock<IAttributeService>();
+        _methodServiceMock = new Mock<IMethodService>();
+        _classServiceMock = new Mock<IClassService>();
+        _attributeServiceMock = new Mock<IAttributeService>();
 
         return new ClassBuilder(
-            methodServiceMock.Object,
-            classServiceMock.Object,
-            attributeServiceMock.Object);
+            _methodServiceMock.Object,
+            _classServiceMock.Object,
+            _attributeServiceMock.Object);
     }
-
 
     [TestInitialize]
     public void Initialize()
@@ -48,10 +50,11 @@ public class ClassServiceTest
         _methodServiceMock = new Mock<IMethodService>(MockBehavior.Strict);
         _attributeServiceMock = new Mock<IAttributeService>(MockBehavior.Strict);
         _builderStrategyMock = new Mock<IBuilderStrategy>(MockBehavior.Strict);
+        _classRepositoryMock = new Mock<IRepository<Class>>(MockBehavior.Strict);
 
         var strategies = new List<IBuilderStrategy> { _builderStrategyMock!.Object };
 
-        _classServiceTest = new ClassService(strategies);
+        _classServiceTest = new ClassService(strategies, _classRepositoryMock.Object);
     }
 
     #region CreateClass
@@ -59,13 +62,56 @@ public class ClassServiceTest
     #region Error
 
     [TestMethod]
-    public void CreateClass_WithEmptyName_ThrowsException()
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void CreateClass_WithNullArgs_ThrowsException()
     {
-        Action action = () => new ClassService(null!).CreateClass(Args);
-
-        action.Should().Throw<ArgumentNullException>();
+       _classServiceTest!.CreateClass(null!);
     }
 
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void CreateClass_WithEmptyName_ThrowsException()
+    {
+        var classBuilder = GetMockedBuilder();
+
+        _builderStrategyMock!.Setup(x => x.WhichIsMyBuilder(It.IsAny<CreateClassArgs>())).Returns(true);
+        _builderStrategyMock.Setup(x => x.CreateBuilder()).Returns(classBuilder);
+
+        Args.Name = null;
+        _classServiceTest!.CreateClass(Args);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(NullReferenceException))]
+    public void CreateClass_WithNoMatchingStrategy_ThrowsException()
+    {
+        _builderStrategyMock!.Setup(x => x.WhichIsMyBuilder(It.IsAny<CreateClassArgs>())).Returns(false);
+        _builderStrategyMock.Setup(x => x.CreateBuilder());
+
+        _classServiceTest!.CreateClass(Args);
+    }
+
+
+    [TestMethod]
+    public void CreateClass_WithNullAttributesList_ThrowsArgumentException()
+    {
+        var argsWithNullAttributes = new CreateClassArgs(
+            "TestClass",
+            false,
+            false,
+            false,
+            null!,
+            [],
+            Guid.NewGuid());
+
+        var classBuilder = GetMockedBuilder();
+        _builderStrategyMock!.Setup(x => x.WhichIsMyBuilder(It.IsAny<CreateClassArgs>())).Returns(true);
+        _builderStrategyMock.Setup(x => x.CreateBuilder()).Returns(classBuilder);
+
+        Action action = () => _classServiceTest!.CreateClass(argsWithNullAttributes);
+
+        action.Should().Throw<ArgumentException>();
+    }
 
     #endregion
 
@@ -87,9 +133,35 @@ public class ClassServiceTest
     [TestMethod]
     public void CreateClass_WithNullParent_LeavesNullParent()
     {
-        Action action = () => new ClassService(null!).CreateClass(null!);
+        Args.Parent = null;
+        var classBuilder = GetMockedBuilder();
 
-        action.Should().Throw<ArgumentNullException>();
+        _builderStrategyMock!.Setup(x => x.WhichIsMyBuilder(It.IsAny<CreateClassArgs>())).Returns(true);
+        _builderStrategyMock.Setup(x => x.CreateBuilder()).Returns(classBuilder);
+
+        Action action = () => _classServiceTest!.CreateClass(Args);
+
+        action.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void CreateClass_WithValidParameters_ReturnsClass()
+    {
+        var classBuilder = GetMockedBuilder();
+
+        _builderStrategyMock!.Setup(x => x.WhichIsMyBuilder(It.IsAny<CreateClassArgs>())).Returns(true);
+        _builderStrategyMock.Setup(x => x.CreateBuilder()).Returns(classBuilder);
+
+        var result = _classServiceTest!.CreateClass(Args);
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Class>();
+        result.Name.Should().Be(Args.Name);
+        result.IsAbstract.Should().Be(Args.IsAbstract);
+        result.IsInterface.Should().Be(Args.IsInterface);
+        result.IsSealed.Should().Be(Args.IsSealed);
+        result.Attributes.Should().BeEquivalentTo(Args.Attributes);
+        result.Methods.Should().BeEquivalentTo(Args.Methods);
+        result.Parent.Should().Be(null);
     }
 
     #endregion
