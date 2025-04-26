efadde7119711258782a87b820e1dd1844426f5e
joaquincalvo <joaquincalvo7@gmail.com>
2025-04-25 22:25:45 -0300

    code:dotnet Format

--- tests/ObjectSim.BusinessLogic.Test/AttributeServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/AttributeServiceTest.cs
@@ -221,7 +221,7 @@ public class AttributeServiceTest
             Name = "Attr",
             ClassId = Guid.NewGuid(),
             Visibility = Domain.Attribute.AttributeVisibility.Public,
-            DataType =Domain.ValueType.Create("int")
+            DataType = Domain.ValueType.Create("int")
         };
 
         _service!.Update(dummyAttribute.Id, dummyAttribute);
