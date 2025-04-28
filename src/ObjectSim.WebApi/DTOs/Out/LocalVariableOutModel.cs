e9e243a29b23cf946d61aec6e66b3e979274d7de
Matias Fontes <matifontes13@gmail.com>
2025-04-28 17:10:11 -0300

    code: implemented Parameter and LocalVariable out model

---
+++ src/ObjectSim.WebApi/DTOs/Out/LocalVariableOutModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ObjectSim.WebApi.DTOs.Out;
+
+public class LocalVariableOutModel
+{
+    public required string Name { get; init; }
+    public required string Type { get; init; }
+
+    public static LocalVariableOutModel ToInfo(ObjectSim.Domain.Parameter parameter)
+    {
+        return new LocalVariableOutModel
+        {
+            Name = parameter.Name!,
+            Type = parameter.Type.ToString()
+        };
+    }
+}
---
+++ src/ObjectSim.WebApi/DTOs/Out/ParameterOutModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ObjectSim.WebApi.DTOs.Out;
+
+public class ParameterOutModel
+{
+    public required string Name { get; init; }
+    public required string Type { get; init; }
+
+    public static ParameterOutModel ToInfo(ObjectSim.Domain.Parameter parameter)
+    {
+        return new ParameterOutModel
+        {
+            Name = parameter.Name!,
+            Type = parameter.Type.ToString()
+        };
+    }
+}
