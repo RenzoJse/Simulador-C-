afdc8746bc67e353020c39046a50e7110ce990c1
RenzoJse <renzojose007@gmail.com>
2025-04-28 14:49:27 -0300

    Code: DeleteClass Controller [RED]

--- tests/ObjectSim.WebApi.Test/Controllers/ClassControllerTest.cs
+++ tests/ObjectSim.WebApi.Test/Controllers/ClassControllerTest.cs
@@ -92,8 +92,56 @@ public class ClassControllerTest
         answer.Attributes.Should().BeEquivalentTo(_testClass.Attributes!.Select(attribute => attribute.Name));
         answer.Methods.Should().BeEquivalentTo(_testClass.Methods!.Select(method => method.Name));
         answer.Parent.Should().Be(_testClass.Parent?.Id);
+        answer.Id.Should().Be(_testClass.Id);
     }
 
     #endregion
 
+    #region GetClass-GET
+
+    [TestMethod]
+    public void GetClass_WhenIsValid_MakesValidGet()
+    {
+        var classId = Guid.NewGuid();
+        _classServiceMock
+            .Setup(service => service.GetById(classId))
+            .Returns(_testClass);
+
+        var result = _classController.GetClass(classId);
+
+        var resultObject = result as OkObjectResult;
+        var statusCode = resultObject?.StatusCode;
+        statusCode.Should().Be(200);
+
+        var answer = resultObject?.Value as ClassInformationDtoOut;
+        answer.Should().NotBeNull();
+        answer.Name.Should().Be(_testClass.Name);
+        answer.IsAbstract.Should().Be((bool)_testClass.IsAbstract!);
+        answer.IsInterface.Should().Be((bool)_testClass.IsInterface!);
+        answer.IsSealed.Should().Be((bool)_testClass.IsSealed!);
+        answer.Attributes.Should().BeEquivalentTo(_testClass.Attributes!.Select(attribute => attribute.Name));
+        answer.Methods.Should().BeEquivalentTo(_testClass.Methods!.Select(method => method.Name));
+        answer.Parent.Should().Be(_testClass.Parent?.Id);
+        answer.Id.Should().Be(_testClass.Id);
+    }
+
+    #endregion
+
+    #region DeleteClass_Delete
+
+    [TestMethod]
+    public void DeleteClass_WhenIsValid_MakesValidDelete()
+    {
+        var classId = Guid.NewGuid();
+        _classServiceMock
+            .Setup(service => service.DeleteClass(classId));
+
+        var result = _classController.DeleteClass(classId);
+
+        var resultObject = result as OkResult;
+        var statusCode = resultObject?.StatusCode;
+        statusCode.Should().Be(200);
+    }
+
+    #endregion
 }
