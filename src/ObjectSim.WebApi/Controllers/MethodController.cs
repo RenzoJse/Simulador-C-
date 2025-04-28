eedad930b521f962024878ebbf4f69fbcfcdaafb
Matias Fontes <matifontes13@gmail.com>
2025-04-28 17:49:18 -0300

    refactor: Create method [GREEN]

--- src/ObjectSim.WebApi/Controllers/MethodController.cs
+++ src/ObjectSim.WebApi/Controllers/MethodController.cs
@@ -1,8 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ObjectSim.IBusinessLogic;
+using ObjectSim.WebApi.DTOs.In;
+using ObjectSim.WebApi.DTOs.Out;
 
 namespace ObjectSim.WebApi.Controllers;
 
 public class MethodController (IMethodService methodService) : ControllerBase
 {
+    [HttpPost]
+    public IActionResult CreateMethod(MethodDtoIn createMethodDtoIn)
+    {
+        var methodInfo = methodService.Create(createMethodDtoIn.ToEntity());
+
+        var response = new MethodOutModel(methodInfo);
+
+        return Ok(response);
+    }
 }
