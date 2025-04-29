fe07e0acf92be6cd2b8af00ad9405e7f6b96a308
joaquincalvo <joaquincalvo7@gmail.com>
2025-04-28 22:05:56 -0300

    code:green GetAllAttributeController

---
+++ src/ObjectSim.WebApi/Controllers/AttributeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObjectSim.IBusinessLogic;
+
+namespace ObjectSim.WebApi.Controllers;
+[ApiController]
+[Route("api/companies")]
+public class AttributeController(IAttributeService attributeService) : ControllerBase
+{
+    private readonly IAttributeService _attributeService = attributeService;
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var attributes = _attributeService.GetAll();
+        return Ok(attributes);
+    }
+}
--- tests/ObjectSim.WebApi.Test/Controllers/AttributeControllerTest.cs
+++ tests/ObjectSim.WebApi.Test/Controllers/AttributeControllerTest.cs
@@ -1,26 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Moq;
-using ObjectSim.DataAccess.Interface;
 using ObjectSim.IBusinessLogic;
+using ObjectSim.WebApi.Controllers;
 namespace ObjectSim.WebApi.Test.Controllers;
 [TestClass]
 public class AttributeControllerTest
 {
     private Mock<IAttributeService> _attributeServiceMock = null!;
     private AttributeController _attributeController = null!;
-    private Mock<IRepository<Domain.Attribute>> _attributeRepositoryMock = null!;
 
-
-    private static readonly Domain.Attribute TestAttribute = new Domain.Attribute
-    {
-        Name = "TestAttribute",
-    };
     [TestInitialize]
     public void Setup()
     {
-        _attribureServiceMock = new Mock<IAttributeService>();
+        _attributeServiceMock = new Mock<IAttributeService>();
         _attributeController = new AttributeController(_attributeServiceMock.Object);
-        SetupHttpContext();
     }
 
     [TestCleanup]
@@ -31,20 +24,25 @@ public class AttributeControllerTest
     [TestMethod]
     public void GetAll_ShouldReturnAttributes_WhenThereAreElements()
     {
-        var attributes = new List<Domain.Attribute>
-        {
-            new Domain.Attribute { Name = "Attribute1" },
-            new Domain.Attribute { Name = "Attribute2" }
-        };
+        var attributes = new List<ObjectSim.Domain.Attribute>
+            {
+                new ObjectSim.Domain.Attribute { Name = "Attribute1" },
+                new ObjectSim.Domain.Attribute { Name = "Attribute2" }
+            };
 
-        _attributeRepositoryMock.Setup(x => x.GetAll()).Returns(attributes);
+        _attributeServiceMock
+            .Setup(service => service.GetAll())
+            .Returns(attributes);
 
         var result = _attributeController.GetAll() as OkObjectResult;
 
         Assert.IsNotNull(result);
         Assert.AreEqual(200, result.StatusCode);
-        var userList = result.Value as List<Domain.Attribute>;
-        Assert.IsNotNull(userList);
-        Assert.AreEqual(2, userList.Count);
+
+        var returnedAttributes = result.Value as List<ObjectSim.Domain.Attribute>;
+        Assert.IsNotNull(returnedAttributes);
+        Assert.AreEqual(2, returnedAttributes.Count);
+        Assert.AreEqual("Attribute1", returnedAttributes[0].Name);
+        Assert.AreEqual("Attribute2", returnedAttributes[1].Name);
     }
 }
