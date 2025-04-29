d2f81aee81d3e37f9ba8439befc981da40788ae3
RenzoJse <renzojose007@gmail.com>
2025-04-29 13:31:53 -0300

    Code: Created IDataTypeService

---
+++ src/ObjectSim.IBusinessLogic/IDataTypeService.cs
@@ -0,0 +1,9 @@
+﻿using ObjectSim.Domain;
+using Attribute = System.Attribute;
+
+namespace ObjectSim.IBusinessLogic;
+
+public interface IDataTypeService
+{
+    IDataType CreateDataType(Attribute dataType);
+}
