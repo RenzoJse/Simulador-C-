f4173ca1deb31b24ef0810616774f56d673f1c5d
joaquincalvo <joaquincalvo7@gmail.com>
2025-05-01 16:28:36 -0300

    code: red CreateDataType

--- src/ObjectSim.BusinessLogic/DataTypeService.cs
+++ src/ObjectSim.BusinessLogic/DataTypeService.cs
@@ -1,10 +1,11 @@
-﻿using ObjectSim.Domain;
+﻿using ObjectSim.DataAccess.Interface;
+using ObjectSim.Domain;
 using ObjectSim.Domain.Args;
 using ObjectSim.IBusinessLogic;
 
 namespace ObjectSim.BusinessLogic;
 
-public class DataTypeService : IDataTypeService
+public class DataTypeService(IRepository<Class> classRepository) : IDataTypeService
 {
     public DataType CreateDataType(CreateDataTypeArgs dataType)
     {
---
+++ tests/ObjectSim.BusinessLogic.Test/DataTypeServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using ObjectSim.DataAccess.Interface;
+using ObjectSim.Domain;
+using ObjectSim.Domain.Args;
+
+namespace ObjectSim.BusinessLogic.Test;
+[TestClass]
+public class DataTypeServiceTest
+{
+    private Mock<IRepository<Class>> _classRepoMock = null!;
+    private DataTypeService _service = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _classRepoMock = new Mock<IRepository<Class>>();
+        _service = new DataTypeService(_classRepoMock.Object);
+    }
+
+    [TestMethod]
+    public void CreateDataType_ReferenceType_ObjectWithRegisteredClass_ShouldSucceed()
+    {
+        var args = new CreateDataTypeArgs("object", "Reference");
+
+        _classRepoMock
+            .Setup(r => r.GetAll(It.IsAny<Func<Class, bool>>()))
+            .Returns([new Class { Name = "object" }]);
+
+        var result = _service.CreateDataType(args);
+
+        Assert.IsInstanceOfType(result, typeof(ReferenceType));
+        Assert.AreEqual("object", result.Name);
+    }
+}
