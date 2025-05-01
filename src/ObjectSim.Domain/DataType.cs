8e8452ec0407b6a2b42f5ea1000872dbb7ae973d
RenzoJse <renzojose007@gmail.com>
2025-04-30 21:50:32 -0300

    Code: ValueType_ShouldInitializeId [GREEN]

--- src/ObjectSim.Domain/DataType.cs
+++ src/ObjectSim.Domain/DataType.cs
@@ -1,6 +1,6 @@
 ﻿namespace ObjectSim.Domain;
 public abstract class DataType
 {
-    public Guid Id { get; init; }
+    public Guid Id { get; init; } = Guid.NewGuid();
     public string Name { get; protected init; } = null!;
 }
--- src/ObjectSim.Domain/ValueType.cs
+++ src/ObjectSim.Domain/ValueType.cs
@@ -2,12 +2,14 @@
 public class ValueType : DataType
 {
     public static readonly List<string> BuiltinTypes = ["int", "bool", "char", "decimal", "DateTime"];
-    private ValueType(string name)
+
+    public ValueType(string name, string type, List<Method> methods)
     {
         Name = name;
     }
+
     public static ValueType Create(string name)
     {
-        return !BuiltinTypes.Contains(name) ? throw new ArgumentException($"Invalid ValueType: {name}") : new ValueType(name);
+        return null!;
     }
 }
