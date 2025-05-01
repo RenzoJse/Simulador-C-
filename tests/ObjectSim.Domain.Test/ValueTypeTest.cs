9a23833a54257b240db030b2df109455f53a200c
Renzo Jose <renzojose007@gmail.com>
2025-05-01 00:08:02 -0300

    Code: CreateValueType_WhenNameHasNumbers_ShouldThrowArgumentException [RED]

--- tests/ObjectSim.Domain.Test/ValueTypeTest.cs
+++ tests/ObjectSim.Domain.Test/ValueTypeTest.cs
@@ -89,6 +89,20 @@ public class ValueTypeTest
         action.Should().Throw<ArgumentException>()
             .WithMessage("Name cannot contain special characters.");
     }
+    
+    [TestMethod]
+    public void CreateValueType_WhenNameHasNumbers_ShouldThrowArgumentException()
+    {
+        const string name = "Invalid123Name";
+
+        Action action = () =>
+        {
+            var valueType = new ValueType(name, _validType, _emptyMethods);
+        };
+
+        action.Should().Throw<ArgumentException>()
+            .WithMessage("Name cannot contain special characters.");
+    }
 
     #endregion
 
