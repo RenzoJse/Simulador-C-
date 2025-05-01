68421996a62c79f100e40554b8bd7aeca00f598f
RenzoJse <renzojose007@gmail.com>
2025-04-30 21:48:08 -0300

    Code: ValueType_ShouldInitializeId [RED]

--- tests/ObjectSim.Domain.Test/DataTypeTest.cs
+++ tests/ObjectSim.Domain.Test/DataTypeTest.cs
@@ -1,6 +1,15 @@
-﻿namespace ObjectSim.Domain.Test;
+﻿using FluentAssertions;
 
+namespace ObjectSim.Domain.Test;
+
+[TestClass]
 public class DataTypeTest
 {
-    
+    [TestMethod]
+    public void ValueType_ShouldInitializeId()
+    {
+        var valueType = new ValueType("ValidName", "int", []);
+
+        valueType.Id.Should().NotBeEmpty();
+    }
 }
