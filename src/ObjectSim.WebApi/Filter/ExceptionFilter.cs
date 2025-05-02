53a0d7f83ef84eb54cc898600450bbe331f9fab4
Matias Fontes <matifontes13@gmail.com>
2025-05-02 18:53:57 -0300

    code: implemented exceptions mappings

--- src/ObjectSim.WebApi/Filter/ExceptionFilter.cs
+++ src/ObjectSim.WebApi/Filter/ExceptionFilter.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ObjectSim.WebApi.Filter;
 
 public sealed class ExceptionFilter : IExceptionFilter
 {
+    private static readonly Dictionary<Type, (HttpStatusCode StatusCode, string InnerCode)> ExceptionMappings = new()
+    {
+        { typeof(ArgumentNullException), (HttpStatusCode.BadRequest, "ArgumentNull") },
+        { typeof(ArgumentException), (HttpStatusCode.BadRequest, "InvalidArgument") },
+        { typeof(InvalidOperationException), (HttpStatusCode.BadRequest, "InvalidOperation") },
+        { typeof(KeyNotFoundException), (HttpStatusCode.NotFound, "NotFound") }
+    };
+
     public void OnException(ExceptionContext context)
     {
 
