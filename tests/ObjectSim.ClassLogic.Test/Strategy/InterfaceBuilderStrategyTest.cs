67599358c1adb22d15289b64ec51290136933303
Renzo Jose <renzojose007@gmail.com>
2025-05-02 12:40:46 -0300

    Code: Created InterfaceBuilderStrategy and tests

---
+++ tests/ObjectSim.ClassLogic.Test/Strategy/InterfaceBuilderStrategyTest.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using ObjectSim.ClassLogic.Strategy;
+using ObjectSim.Domain.Args;
+using ObjectSim.IBusinessLogic;
+
+namespace ObjectSim.ClassLogic.Test.Strategy;
+
+[TestClass]
+public class InterfaceBuilderStrategyTest
+{
+    private Mock<IAttributeService>? _attributeServiceMock;
+    private Mock<IClassService>? _classServiceMock;
+    private InterfaceBuilderStrategy? _strategy;
+    
+    [TestInitialize]
+    public void Initialize()
+    {
+        _attributeServiceMock = new Mock<IAttributeService>();
+        _classServiceMock = new Mock<IClassService>();
+
+        _strategy = new InterfaceBuilderStrategy(_attributeServiceMock.Object);
+    }
+    
+    #region WhichIsMyBuilder
+
+    [TestMethod]
+    public void WhichIsMyBuilder_WithNormalClass_ReturnsFalse()
+    {
+        var args = new CreateClassArgs("NormalClass", false, false, false, [], [], null);
+
+        var result = _strategy!.WhichIsMyBuilder(args);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void WhichIsMyBuilder_WithInterfaceClass_ReturnsTrue()
+    {
+        var args = new CreateClassArgs("InterfaceClass", false, false, true, [], [], null);
+
+        var result = _strategy!.WhichIsMyBuilder(args);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void WhichIsMyBuilder_WithAbstractClass_ReturnsFalse()
+    {
+        var args = new CreateClassArgs("AbstractClass", true, false, false, [], [], null);
+
+        var result = _strategy!.WhichIsMyBuilder(args);
+
+        Assert.IsFalse(result);
+    }
+
+    #endregion
+}
