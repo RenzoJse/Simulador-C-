3454c71410ea4515e849f57c7b8abd0dafdf3f41
Matias Fontes <matifontes13@gmail.com>
2025-05-02 18:53:11 -0300

    code: create exception filter class

---
+++ src/ObjectSim.WebApi/Filter/ExceptionFilter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ObjectSim.WebApi.Filter;
+
+public sealed class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+
+    }
+}
