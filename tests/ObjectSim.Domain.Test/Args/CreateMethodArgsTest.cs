c93830f08f2bb5435970f5ea0c763253b5978f33
RenzoJse <renzojose007@gmail.com>
2025-05-04 11:52:41 -0300

    Code: Created test for CreateMethodArgs

---
+++ tests/ObjectSim.Domain.Test/Args/CreateMethodArgsTest.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using ObjectSim.Domain.Args;
+
+namespace ObjectSim.Domain.Test.Args;
+
+[TestClass]
+public class CreateMethodArgsTest
+{
+    [TestMethod]
+    public void Name_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        const string newName = "Other";
+        args.Name = newName;
+        args.Name.Should().Be(newName);
+    }
+
+    [TestMethod]
+    public void Type_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var type = new CreateDataTypeArgs("Type", "int");
+        var args = new CreateMethodArgs("Test", type, "public", true, false, true, Guid.NewGuid(), [], [], []);
+        var newType = new CreateDataTypeArgs("OtherType", "float");
+        args.Type = newType;
+        args.Type.Should().Be(newType);
+    }
+
+    [TestMethod]
+    public void Accessibility_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        const string newAccessibility = "private";
+        args.Accessibility = newAccessibility;
+        args.Accessibility.Should().Be(newAccessibility);
+    }
+
+    [TestMethod]
+    public void IsAbstract_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        args.IsAbstract = false;
+        args.IsAbstract.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void IsSealed_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], [])
+        {
+            IsSealed = true
+        };
+        args.IsSealed.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void IsOverride_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], [])
+        {
+            IsOverride = false
+        };
+        args.IsOverride.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void ClassId_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        var newId = Guid.NewGuid();
+        args.ClassId = newId;
+        args.ClassId.Should().Be(newId);
+    }
+
+    [TestMethod]
+    public void LocalVariables_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        var newVars = new List<CreateDataTypeArgs> { new("var", "int") };
+        args.LocalVariables = newVars;
+        args.LocalVariables.Should().BeEquivalentTo(newVars);
+    }
+
+    [TestMethod]
+    public void Parameters_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        var newParams = new List<CreateDataTypeArgs> { new("param", "string") };
+        args.Parameters = newParams;
+        args.Parameters.Should().BeEquivalentTo(newParams);
+    }
+
+    [TestMethod]
+    public void InvokeMethods_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var args = new CreateMethodArgs("Test", new CreateDataTypeArgs("Type", "int"), "public", true, false, true, Guid.NewGuid(), [], [], []);
+        var newInvoke = new List<Guid> { Guid.NewGuid() };
+        args.InvokeMethods = newInvoke;
+        args.InvokeMethods.Should().BeEquivalentTo(newInvoke);
+    }
+}
