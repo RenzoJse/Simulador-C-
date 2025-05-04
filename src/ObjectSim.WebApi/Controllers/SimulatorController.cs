cf2d0f9978c71f084011057d749e2be9bde216d2
Matias Fontes <matifontes13@gmail.com>
2025-05-04 18:12:31 -0300

    code: implemented simulator controller [GREEN]

---
+++ src/ObjectSim.WebApi/Controllers/SimulatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObjectSim.Domain.Args;
+using ObjectSim.IBusinessLogic;
+using ObjectSim.WebApi.Filter;
+
+namespace ObjectSim.WebApi.Controllers;
+
+[ApiController]
+[Route("api/simulator")]
+[TypeFilter(typeof(ExceptionFilter))]
+public class SimulatorController(IMethodSimulatorService simulatorService) : ControllerBase
+{
+    [HttpPost]
+
+    public IActionResult SimulateExecution([FromBody] SimulateExecutionArgs args)
+    {
+        var result = simulatorService.Simulate(args);
+        return Ok(result);
+    }
+}
