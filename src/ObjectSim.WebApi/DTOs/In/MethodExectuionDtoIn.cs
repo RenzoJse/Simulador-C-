dcb650ffd9aba3d21cf0d293d2ba379a0a12a5ef
Matias Fontes <matifontes13@gmail.com>
2025-05-04 18:02:20 -0300

    remove unnecessary dtos

--- src/ObjectSim.WebApi/DTOs/In/MethodExectuionDtoIn.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ObjectSim.WebApi.DTOs.In;
-
-public class MethodExecutionDtoIn
-{
-    public Guid ReferenceClassId { get; init; }
-    public Guid InstanceClassId { get; init; }
-    public Guid MethodId { get; init; }
-}
--- src/ObjectSim.WebApi/DTOs/Out/ExecutionStepDtoOut.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ObjectSim.WebApi.DTOs.Out;
-
-public class ExecutionStepDtoOut
-{
-    public string Caller { get; init; } = string.Empty;
-    public string MethodName {  get; init; } = string.Empty;
-    public List<ExecutionStepDtoOut> NestedCalls { get; init; } = [];
-}
