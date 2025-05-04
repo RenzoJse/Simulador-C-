41b0ce477a69cc237d3268bd63f98f6c577235d2
Matias Fontes <matifontes13@gmail.com>
2025-05-04 17:12:02 -0300

    code: implemented Exectuion methods dto out

---
+++ src/ObjectSim.WebApi/DTOs/Out/ExecutionStepDtoOut.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectSim.WebApi.DTOs.Out;
+
+public class ExecutionStepDtoOut
+{
+    public string Caller { get; init; } = string.Empty;
+    public string MethodName {  get; init; } = string.Empty;
+    public List<ExecutionStepDtoOut> NestedCalls { get; init; } = [];
+}
