5fe5275ddbae418d75ef68fd3a159f28f5374d54
Matias Fontes <matifontes13@gmail.com>
2025-05-04 17:19:48 -0300

    code: implemented simulate execution Args

---
+++ src/ObjectSim.Domain/Args/SimulateExecutionArgs.cs
@@ -0,0 +1,7 @@
+﻿namespace ObjectSim.Domain.Args;
+public class SimulateExecutionArgs
+{
+    public Guid ReferenceClassId { get; set; }
+    public Guid InstanceClassId { get; set; }
+    public Guid MethodToExecuteId {  get; set; }
+}
