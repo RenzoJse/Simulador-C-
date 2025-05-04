111bd6fdb2eaf02f99c3b6a06fbfa2d71017f11f
Matias Fontes <matifontes13@gmail.com>
2025-05-04 17:08:37 -0300

    code: implemented MethodExectuionDtoIn

---
+++ src/ObjectSim.WebApi/DTOs/In/MethodExectuionDtoIn.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectSim.WebApi.DTOs.In;
+
+public class SimulateMethodExecutionDtoIn
+{
+    public Guid ReferenceClassId { get; init; }
+    public Guid InstanceClassId { get; init; }
+    public Guid MethodId { get; init; }
+}
