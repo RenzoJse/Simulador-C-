b31e91f049ff3ea95a07170b565b1878c5efd32c
RenzoJse <renzojose007@gmail.com>
2025-05-06 17:58:47 -0300

    Code: CreateInvokeMethodDtoInTest

---
+++ tests/ObjectSim.WebApi.Test/Models/In/CreateInvokeMethodDtoInTest.cs
@@ -0,0 +1,27 @@
+﻿namespace ObjectSim.WebApi.Test.Models.In;
+
+[TestClass]
+public class CreateInvokeMethodDtoInTest
+{
+
+    [TestMethod]
+    public void CreateDataTypeDtoIn_WithArguments_ReturnsNewInstance()
+    {
+        var model = new CreateInvokeMethodDtoIn { MethodId = Guid.NewGuid(), InvokeMethodId = Guid.NewGuid(), Reference = "init" };
+        model.MethodId.Should().NotBe(Guid.Empty);
+        model.InvokeMethodId.Should().NotBe(Guid.Empty);
+        model.Reference.Should().Be("init");
+    }
+
+    [TestMethod]
+    public void CreateDataTypeDtoInToArgs_WithArguments_DataTypeArgs()
+    {
+        var model = new CreateInvokeMethodDtoIn { MethodId = Guid.NewGuid(), InvokeMethodId = Guid.NewGuid(), Reference = "init" };
+        var args = model.ToArgs();
+        args.Should().NotBeNull();
+        args.MethodId.Should().NotBe(Guid.Empty);
+        args.InvokeMethodId.Should().NotBe(Guid.Empty);
+        args.Reference.Should().Be("init");
+    }
+
+}
