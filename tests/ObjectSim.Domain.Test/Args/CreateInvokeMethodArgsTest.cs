d975a2f89fc509d2372d08d9c759f3809ceb17bd
Renzo Jose <renzojose007@gmail.com>
2025-05-05 21:15:11 -0300

    Code: InvokeMethodId_SetAndGet_ShouldReturnSameValue [RED]

--- tests/ObjectSim.Domain.Test/Args/CreateInvokeMethodArgsTest.cs
+++ tests/ObjectSim.Domain.Test/Args/CreateInvokeMethodArgsTest.cs
@@ -1,6 +1,16 @@
 ﻿namespace ObjectSim.Domain.Test.Args;
 
+[TestClass]
 public class CreateInvokeMethodArgsTest
 {
-    
+    [TestMethod]
+    public void InvokeMethodId_SetAndGet_ShouldReturnSameValue()
+    {
+        var invokeMethodArgs = new CreateInvokeMethodArgs(Guid.NewGuid(), "Test", Guid.NewGuid());
+        var expectedId = Guid.NewGuid();
+        
+        invokeMethodArgs.InvokeMethodId = expectedId;
+        
+        Assert.AreEqual(expectedId, invokeMethodArgs.InvokeMethodId);
+    }
 }
