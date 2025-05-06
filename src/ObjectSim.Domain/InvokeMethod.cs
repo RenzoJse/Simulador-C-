ac7f00248d3c2e8a6805e3f85a077cb74a524053
Renzo Jose <renzojose007@gmail.com>
2025-05-05 21:38:57 -0300

    Code: Reference_SetAndGet_ShouldReturnExpectedValue [GREEN]

--- src/ObjectSim.Domain/InvokeMethod.cs
+++ src/ObjectSim.Domain/InvokeMethod.cs
@@ -1,7 +1,8 @@
 ﻿namespace ObjectSim.Domain;
 
-public class InvokeMethod(Guid methodId, Guid invokeMethodId)
+public class InvokeMethod(Guid methodId, Guid invokeMethodId, string reference)
 {
     public Guid MethodId { get; set; } = methodId;
     public Guid InvokeMethodId { get; set; } = invokeMethodId;
+    public string Reference { get; set; } = reference;
 }
