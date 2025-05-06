56cc8f407f56069713309d3702060115b4682b74
Renzo Jose <renzojose007@gmail.com>
2025-05-05 21:38:30 -0300

    Code: Reference_SetAndGet_ShouldReturnExpectedValue [RED]

--- tests/ObjectSim.Domain.Test/InvokeMethodTest.cs
+++ tests/ObjectSim.Domain.Test/InvokeMethodTest.cs
@@ -8,7 +8,7 @@ public class InvokeMethodTest
     [TestMethod]
     public void MethodId_SetAndGet_ShouldReturnExpectedValue()
     {
-        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid());
+        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid(), "Test");
         var newId = Guid.NewGuid();
         
         invokeMethod.MethodId = newId;
@@ -19,11 +19,22 @@ public class InvokeMethodTest
     [TestMethod]
     public void InvokeMethodId_SetAndGet_ShouldReturnExpectedValue()
     {
-        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid());
+        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid(), "Test");
         var newId = Guid.NewGuid();
         
         invokeMethod.InvokeMethodId = newId;
      
         invokeMethod.InvokeMethodId.Should().Be(newId);
+    }
+    
+    [TestMethod]
+    public void Reference_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid(), "Test");
+        const string newReference = "NewReference";
+        
+        invokeMethod.Reference = newReference;
+        
+        invokeMethod.Reference.Should().Be(newReference);
     }
 }
