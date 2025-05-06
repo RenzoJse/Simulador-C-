c8f42013db7b0af5a492c493ad0a33badc7ce5fa
Renzo Jose <renzojose007@gmail.com>
2025-05-05 21:34:40 -0300

    Code: InvokeMethodId_SetAndGet_ShouldReturnExpectedValue [RED]

--- tests/ObjectSim.Domain.Test/InvokeMethodTest.cs
+++ tests/ObjectSim.Domain.Test/InvokeMethodTest.cs
@@ -8,11 +8,22 @@ public class InvokeMethodTest
     [TestMethod]
     public void MethodId_SetAndGet_ShouldReturnExpectedValue()
     {
-        var invokeMethod = new InvokeMethod(Guid.NewGuid());
+        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid());
         var newId = Guid.NewGuid();
         
         invokeMethod.MethodId = newId;
         
         invokeMethod.MethodId.Should().Be(newId);
+    }
+    
+    [TestMethod]
+    public void InvokeMethodId_SetAndGet_ShouldReturnExpectedValue()
+    {
+        var invokeMethod = new InvokeMethod(Guid.NewGuid(), Guid.NewGuid());
+        var newId = Guid.NewGuid();
+        
+        InvokeMethod.InvokeMethodId = newId;
+     
+        invokeMethod.InvokeMethodId.Should().Be(newId);
     }
 }
