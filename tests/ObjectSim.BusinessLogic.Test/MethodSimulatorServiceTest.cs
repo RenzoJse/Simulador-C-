b5b023c2e9601e3f1bf4e89313be7f766a0a7bcb
RenzoJse <renzojose007@gmail.com>
2025-05-07 14:45:54 -0300

    Code: Simulate_WhenArgumentsAreValid_Executes [GREEN]

--- tests/ObjectSim.BusinessLogic.Test/MethodSimulatorServiceTest.cs
+++ tests/ObjectSim.BusinessLogic.Test/MethodSimulatorServiceTest.cs
@@ -172,6 +172,82 @@ public class MethodSimulatorServiceTest
         act.Should().Throw<Exception>().WithMessage("Method not found in reference type");
     }
 
+    [TestMethod]
+    public void Simulate_WhenMethodDoesNotExist_ThrowsException()
+    {
+        var methodId = Guid.NewGuid();
+        var args = new SimulateExecutionArgs
+        {
+            ReferenceType = "ReferenceType",
+            InstanceType = "ReferenceType",
+            MethodId = methodId
+        };
+
+        var referenceType = new ReferenceType("ReferenceType", "ReferenceType", []);
+
+        _dataTypeRepositoryMock.SetupSequence(r => r.Get(It.IsAny<Func<DataType, bool>>()))
+            .Returns(referenceType)
+            .Returns(referenceType);
+
+        _methodRepositoryMock.Setup(r => r.Get(It.IsAny<Func<Method, bool>>()))
+            .Returns((Method)null!);
+
+        Action act = () => _methodSimulatorServiceTest.Simulate(args);
+
+        act.Should().Throw<Exception>().WithMessage("Method not found");
+    }
+
+    #endregion
+
+    #region Success
+
+    [TestMethod]
+    public void Simulate_WhenArgumentsAreValid_Executes()
+    {
+        var methodId = Guid.NewGuid();
+        var invokeMethodId1 = Guid.NewGuid();
+        var invokeMethodId2 = Guid.NewGuid();
+
+        var args = new SimulateExecutionArgs
+        {
+            ReferenceType = "ReferenceType", InstanceType = "ReferenceType", MethodId = methodId
+        };
+
+        var method = new Method
+        {
+            Id = methodId,
+            Name = "MainMethod",
+            MethodsInvoke =
+            [
+                new InvokeMethod(methodId, invokeMethodId1, "this"),
+                new InvokeMethod(methodId, invokeMethodId2, "other"),
+            ]
+        };
+
+        var invokedMethod1 = new Method { Id = invokeMethodId1, Name = "FirstInvoked" };
+        var invokedMethod2 = new Method { Id = invokeMethodId2, Name = "SecondInvoked" };
+
+        var referenceType = new ReferenceType("ReferenceType", "ReferenceType", [methodId]);
+
+        _dataTypeRepositoryMock.SetupSequence(r => r.Get(It.IsAny<Func<DataType, bool>>()))
+            .Returns(referenceType)
+            .Returns(referenceType);
+
+        var classObj = new Class { Name = "ReferenceType", Methods = new List<Method> { method } };
+
+        _classRepositoryMock.Setup(r => r.Get(It.IsAny<Func<Class, bool>>()))
+            .Returns(classObj);
+
+        _methodRepositoryMock.Setup(r => r.Get(It.IsAny<Func<Method, bool>>()))
+            .Returns((Func<Method, bool> predicate) =>
+                new[] { method, invokedMethod1, invokedMethod2 }.FirstOrDefault(predicate));
+
+        List<string> result = _methodSimulatorServiceTest.Simulate(args);
+
+        result.Should().HaveCount(2);
+        result[0].Should().Be("this.MainMethod() ->");
+    }
+
     #endregion
 
     #endregion
