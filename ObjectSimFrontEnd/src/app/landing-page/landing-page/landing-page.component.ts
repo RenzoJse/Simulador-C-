4d62fd635abae609c72fb29b034e251c3033b3b1
RenzoJse <renzojose007@gmail.com>
2025-05-21 20:34:44 -0300

    Code: Added missing module

---
+++ ObjectSimFrontEnd/src/app/landing-page/landing-page/landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  template: `
+    <p>
+      landing-page works!
+    </p>
+  `,
+  templateUrl: "./landing-page.component.html",
+})
+
+export class LandingPageComponent {
+
+}
