108c65ab1411583cbd7f136f50a2db25bcea73b9
joaquincalvo <joaquincalvo7@gmail.com>
2025-05-23 00:51:14 -0300

    code:red CreateNamespaceOK

---
+++ tests/ObjectSim.BusinessLogic.Test/NamespaceServiceTest.cs
@@ -0,0 +1,29 @@
+﻿
+using Moq;
+using ObjectSim.DataAccess.Interface;
+
+namespace ObjectSim.BusinessLogic.Test;
+[TestClass]
+public class NamespaceServiceTest
+{
+    private NamespaceService _namespaceService;
+    private Mock<IRepository<Namespace>> _namespaceRepositoryMock;
+    [TestInitialize]
+    public void Setup()
+    {
+        _namespaceRepositoryMock = new Mock<IRepository<Namespace>>();
+        _namespaceService = new NamespaceService(_namespaceRepositoryMock.Object);
+    }
+    [TestMethod]
+    public void Create_WithValidNamespace_CallsAddOnRepository()
+    {
+        // Arrange
+        var ns = new Namespace { Name = "Utilities" };
+
+        // Act
+        _service.Create(ns);
+
+        // Assert
+        _namespaceRepoMock.Verify(r => r.Add(ns), Times.Once);
+    }
+}
