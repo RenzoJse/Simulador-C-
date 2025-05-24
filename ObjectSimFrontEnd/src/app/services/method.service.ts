0fac3112e4d6c90cab7117b9c39d7eb962157921
Matias Fontes <matifontes13@gmail.com>
2025-05-24 18:03:02 -0300

    code: implemented addmethod service

--- ObjectSimFrontEnd/src/app/services/method.service.ts
+++ ObjectSimFrontEnd/src/app/services/method.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MethodDTO } from '../models/method-dto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MethodService {
+  private readonly apiUrl = '/api/methods';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createMethod(method: MethodDTO): Observable<any> {
+    return this.http.post(this.apiUrl, method);
+  }
 }
