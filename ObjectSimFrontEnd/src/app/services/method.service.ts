0c3993ef1f21e567562136cd09919009dc332fd2
Matias Fontes <matifontes13@gmail.com>
2025-05-24 18:25:09 -0300

    code: implemented method service

--- ObjectSimFrontEnd/src/app/services/method.service.ts
+++ ObjectSimFrontEnd/src/app/services/method.service.ts
@@ -7,7 +7,7 @@ import { MethodDTO } from '../models/method-dto.model';
   providedIn: 'root'
 })
 export class MethodService {
-  private readonly apiUrl = '/api/methods';
+  private readonly apiUrl = 'http://localhost:5000/api/methods';
 
   constructor(private http: HttpClient) {}
 
