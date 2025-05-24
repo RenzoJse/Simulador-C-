864b78bea13e5435d9a2d992803de20dd67e23f0
joaquincalvo <joaquincalvo7@gmail.com>
2025-05-24 02:46:28 -0300

    code:red GetAllDescendantsNamespace

--- tests/ObjectSim.Domain.Test/NamespaceTest.cs
+++ tests/ObjectSim.Domain.Test/NamespaceTest.cs
@@ -24,4 +24,23 @@ public class NamespaceTest
 
         Assert.IsFalse(parent.Children.Contains(child));
     }
+    [TestMethod]
+    public void GetAllDescendants_ShouldReturnAllNestedChildren()
+    {
+        var root = new Namespace { Name = "Root" };
+        var child1 = new Namespace { Name = "Child1" };
+        var child2 = new Namespace { Name = "Child2" };
+        var grandChild = new Namespace { Name = "GrandChild" };
+
+        child1.AddChild(grandChild);
+        root.AddChild(child1);
+        root.AddChild(child2);
+
+        var descendants = root.GetAllDescendants().ToList();
+
+        Assert.AreEqual(3, descendants.Count);
+        CollectionAssert.Contains(descendants, child1);
+        CollectionAssert.Contains(descendants, child2);
+        CollectionAssert.Contains(descendants, grandChild);
+    }
 }
