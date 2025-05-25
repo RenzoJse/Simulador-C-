414610b6ac71336f461bec9b5739744947ae967e
RenzoJse <renzojose007@gmail.com>
2025-05-25 18:56:27 -0300

    Code: Added new inputs to create Method

--- ObjectSimFrontEnd/src/business-components/create-method-form/create-method-form.component.ts
+++ ObjectSimFrontEnd/src/business-components/create-method-form/create-method-form.component.ts
@@ -35,6 +35,9 @@ export class CreateMethodFormComponent {
             type: ['', [
                 Validators.required
             ]],
+            classId: ['', [
+                Validators.required
+            ]]
         });
     }
 
