c864c649e79d83040de9236db5c47d5337f805dc
Renzo Jose <renzojose007@gmail.com>
2025-05-26 19:56:04 -0300

    Code: Created create class form html

---
+++ ObjectSimFrontEnd/src/business-components/create-class-form/create-class-form.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+import { FormInputComponent } from '../../components/form/form-input/form-input.component';
+import { FormButtonComponent } from '../../components/form/form-button/form-button.component';
+import { FormComponent } from '../../components/form/form/form.component';
+
+@Component({
+    selector: 'app-create-class-form',
+    standalone: true,
+    imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent, NgIf, FormComponent],
+    templateUrl: './create-class-form.component.html'
+})
