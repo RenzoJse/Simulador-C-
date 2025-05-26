f363b4fac98e6e14df3a67fa4913f2b59ac19f49
Renzo Jose <renzojose007@gmail.com>
2025-05-26 20:06:48 -0300

    Code: Created class module and create class component

--- ObjectSimFrontEnd/src/business-components/create-class-form/create-class-form.component.ts
+++ ObjectSimFrontEnd/src/business-components/create-class-form/create-class-form.component.ts
@@ -12,3 +12,52 @@ import { FormComponent } from '../../components/form/form/form.component';
     imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent, NgIf, FormComponent],
     templateUrl: './create-class-form.component.html'
 })
+
+export class CreateClassFormComponent {
+    @Input() title: string = '';
+
+    createClassForm: FormGroup;
+    @Output() atSubmit = new EventEmitter<any>();
+
+    createClassStatus: {
+        loading?: true;
+        error?: string;
+    } | null = null;
+
+    Modificadores: { value: string; tag: string }[] = [
+        { value: 'Abstract', tag: 'Abstract' },
+        { value: 'Interface', tag: 'Interface' },
+        { value: 'Sealed', tag: 'Sealed' }
+    ];
+
+    constructor(private fb: FormBuilder) {
+        this.createClassForm = this.fb.group({
+            Name: ['', [
+                Validators.required,
+                Validators.maxLength(20),
+                Validators.minLength(3)
+            ]],
+            classType: ['', [Validators.required]],
+        });
+    }
+
+    public onSubmit() {
+        console.log('Form values:', this.createClassForm.value);
+
+        if (this.createClassForm.invalid) {
+            this.markAsTouched();
+            return;
+        }
+
+        this.atSubmit.emit(this.createClassForm.value);
+    }
+
+    private markAsTouched() {
+        Object.keys(this.createClassForm.controls).forEach(field => {
+            const control = this.createClassForm.get(field);
+            if (control) {
+                control.markAsTouched({ onlySelf: true });
+            }
+        });
+    }
+}
