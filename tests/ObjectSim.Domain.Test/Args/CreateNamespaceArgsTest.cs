c06b7635abe2a76b57dc7bfd534c86a36cb4d7a6
joaquincalvo <joaquincalvo7@gmail.com>
2025-05-25 23:20:08 -0300

    code:green setArgs

--- tests/ObjectSim.Domain.Test/Args/CreateNamespaceArgsTest.cs
+++ tests/ObjectSim.Domain.Test/Args/CreateNamespaceArgsTest.cs
@@ -1,4 +1,19 @@
-﻿namespace ObjectSim.Domain.Test.Args;
-internal class CreateNamespaceArgsTest
+﻿using ObjectSim.Domain.Args;
+
+namespace ObjectSim.Domain.Test.Args;
+[TestClass]
+public class CreateNamespaceArgsTest
 {
+    [TestMethod]
+    public void Constructor_WithValidValues_ShouldSetProperties()
+    {
+        var name = "MyNamespace";
+        Guid? parentId = Guid.NewGuid();
+
+        var args = new CreateNamespaceArgs(name, parentId);
+
+        Assert.AreEqual(name, args.Name);
+        Assert.AreEqual(parentId, args.ParentId);
+        Assert.AreNotEqual(Guid.Empty, args.Id);
+    }
 }
