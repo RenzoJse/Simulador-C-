973a24abdad1edcf4e3b59c3c2c081c22f767123
Renzo Jose <renzojose007@gmail.com>
2025-05-26 20:18:22 -0300

    Code: Codded class modules

---
+++ ObjectSimFrontEnd/src/app/class/class.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { CreateClassComponent } from './create-class/create-class.component';
+import { ClassRoutingModule } from './class-routing.module';
+
+import { CreateMethodFormComponent } from '../../business-components/create-method-form/create-method-form.component';
+import { ButtonComponent } from '../../components/button/button.component';
+
+@NgModule({
+    declarations: [
+        CreateClassComponent,
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        ClassRoutingModule,
+        CreateMethodFormComponent,
+        ButtonComponent
+    ]
+})
+export class MethodModule { }
