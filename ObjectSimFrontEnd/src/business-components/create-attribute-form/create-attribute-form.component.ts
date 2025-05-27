aa7d7315138bdb72880e39958438a5043d23a6bd
Renzo Jose <renzojose007@gmail.com>
2025-05-27 19:12:27 -0300

    Code: Created component for creating attributes

---
+++ ObjectSimFrontEnd/src/business-components/create-attribute-form/create-attribute-form.component.ts
@@ -0,0 +1,70 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+import { FormInputComponent } from '../../components/form/form-input/form-input.component';
+import { FormButtonComponent } from '../../components/form/form-button/form-button.component';
+import { FormComponent } from '../../components/form/form/form.component';
+
+import AttributeFormComponent from '../../backend/';
+
+@Component({
+    selector: 'app-create-attribute-form',
+    standalone: true,
+    imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent, NgIf, FormComponent],
+    templateUrl: './create-attribute-form.component.html',
+    styleUrls: ['./create-attribute-form.component.css']
+})
+
+export class CreateAttributeFormComponent {
+    @Input() tittle : string = '';
+    
+    createAttributeForm: FormGroup;
+    @Output() attributeCreated = new EventEmitter<void>();
+
+    attributeForm: FormGroup;
+
+    createAttributeStatus: {
+        loading?: true;
+        error?: string;
+    } | null = null;
+
+    Visibility: { value: string; tag: string }[] = [
+        { value: 'Public', tag: 'Public' },
+        { value: 'Private', tag: 'Private' },
+        { value: 'Protected', tag: 'Protected' }
+    ];
+
+    constructor(private formBuilder: FormBuilder) {
+        this.attributeForm = this.formBuilder.group({
+            name: ['', [Validators.required, Validators.maxLength(10)]],
+            dataTypeID: ['', Validators.required],
+            accessibility: ['', Validators.required]
+        });
+    }
+
+    public onSubmit() {
+        console.log('Form attribute submitted:', this.attributeForm.value);
+        
+        if(this.createAttributeForm.invalid) {
+            this.markAsTouched()
+            return;
+        }
+        
+        var selectedModifier = this.attributeForm.value.accessibility;
+        var formValue = this.attributeForm.value;
+        var newAttribute: AttributeCreateModel = {
+            name: formValue.name,
+            dataTypeID: formValue.dataTypeID,
+            accessibility: selectedModifier
+        };
+        
+        this.atSubmit.emit(newAttribute);
+    }
+
+    private markAsTouched() {
+        Object.values(this.createAttributeForm.controls).forEach(control => {
+            control.markAsTouched();
+        });
+    }
+}
--- src/ObjectSim.Domain/Args/CreateAttributeArgs.cs
+++ src/ObjectSim.Domain/Args/CreateAttributeArgs.cs
@@ -1,12 +1,12 @@
 ﻿namespace ObjectSim.Domain.Args;
 
 public class CreateAttributeArgs(
-    CreateDataTypeArgs dataType,
+    Guid dataType,
     string visibility,
     Guid classId,
     string name)
 {
-    public CreateDataTypeArgs DataType { get; set; } = dataType;
+    public Guid DataTypeId { get; set; } = dataType;
     public string Visibility { get; set; } = visibility;
     public Guid Id { get; set; } = Guid.NewGuid();
     public Guid ClassId { get; set; } = classId;
