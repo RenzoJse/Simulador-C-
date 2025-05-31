71f4e404a581088480106585868705218f87aee9
joaquincalvo <joaquincalvo7@gmail.com>
2025-05-31 02:00:53 -0300

    code:delete attribute

--- ObjectSimFrontEnd/src/app/attribute/attribute-routing.module.ts
+++ ObjectSimFrontEnd/src/app/attribute/attribute-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAttributeComponent } from './create-attribute/create-attribute.component';
+import { DeleteAttributeComponent } from './delete-attribute/delete-attribute.component';
 
 const routes: Routes = [
-  { path: 'create', component: CreateAttributeComponent }
+  { path: 'create', component: CreateAttributeComponent },
+  { path: 'delete', component: DeleteAttributeComponent }
 ];
 
 @NgModule({
--- ObjectSimFrontEnd/src/app/attribute/attribute.module.ts
+++ ObjectSimFrontEnd/src/app/attribute/attribute.module.ts
@@ -6,19 +6,22 @@ import { AttributeRoutingModule } from './attribute-routing.module';
 
 import { CreateAttributeComponent } from './create-attribute/create-attribute.component';
 import { CreateAttributeFormComponent } from '../../business-components/create-attribute-form/create-attribute-form.component';
-
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../components/button/button.component';
+import { DeleteAttributeComponent } from './delete-attribute/delete-attribute.component';
 
 @NgModule({
   declarations: [
     CreateAttributeComponent,
+    DeleteAttributeComponent
     ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     AttributeRoutingModule,
     CreateAttributeFormComponent,
-    ButtonComponent
+    ButtonComponent,
+    FormsModule
   ]
 })
 export class AttributeModule {}
