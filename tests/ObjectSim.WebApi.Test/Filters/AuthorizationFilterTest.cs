8b006cc414b102cd68ea86847c0fcdfc20354262
RenzoJse <renzojose007@gmail.com>
2025-06-01 20:36:35 -0300

    Code: OnAuthorization_WhenKeyIsInvalid_ThrowsException [GREEN]

--- tests/ObjectSim.WebApi.Test/Filters/AuthorizationFilterTest.cs
+++ tests/ObjectSim.WebApi.Test/Filters/AuthorizationFilterTest.cs
@@ -1,21 +1,41 @@
 ﻿using System.Net;
 using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Moq;
 using ObjectSim.WebApi.Filter;
+using RouteData = Microsoft.AspNetCore.Routing.RouteData;
 
 namespace ObjectSim.WebApi.Test.Filters;
 
 [TestClass]
 public class AuthorizationFilterTest
 {
-    private AuthorizationFilterContext _context;
-    private AuthorizationFilter _filter;
+    private AuthorizationFilterContext? _context;
+    private AuthorizationFilter? _filter;
+    private readonly Guid _key = Guid.NewGuid();
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _filter = new AuthorizationFilter(_key.ToString());
+
+        var httpContext = new DefaultHttpContext();
+        _context = new AuthorizationFilterContext(
+            new ActionContext(httpContext, new RouteData(), new ActionDescriptor()), []);
+    }
+
+    #region OnAuthorization
+
+    #region Error
 
     [TestMethod]
     public void OnAuthorization_WhenKeyIsInvalid_ThrowsException()
     {
-        _filter.OnAuthorization(_context);
+        _filter!.OnAuthorization(_context!);
 
         var answer = _context!.Result;
         answer.Should().NotBeNull();
@@ -24,5 +44,15 @@ public class AuthorizationFilterTest
         response.Should().NotBeNull();
         response!.StatusCode.Should().Be((int)HttpStatusCode.Unauthorized);
     }
+
+    #endregion
+
+    #region Success
+
+
+
+    #endregion
+
+    #endregion
 
 }
