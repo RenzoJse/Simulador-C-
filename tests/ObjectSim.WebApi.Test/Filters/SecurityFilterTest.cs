2e9de37938dcf02b31c5bd8e527dff4862cba154
RenzoJse <renzojose007@gmail.com>
2025-05-31 22:32:09 -0300

    Code: OnAuthorization_WhenKeyIsInvalid_ThrowsException [RED]

---
+++ tests/ObjectSim.WebApi.Test/Filters/SecurityFilterTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObjectSim.WebApi.Test.Filters;
+
+[TestClass]
+public class SecurityFilterTest
+{
+    private SecurityFilterContext? _context;
+    private SecurityFilter _filter;
+
+    [TestMethod]
+    public void OnAuthorization_WhenKeyIsInvalid_ThrowsException()
+    {
+        _filter.OnAuthorization();
+
+        var answer = _context!.Result;
+        answer.Should().NotBeNull();
+
+        var response = answer as ObjectResult;
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((int)HttpStatusCode.Unauthorized);
+    }
+
+}
