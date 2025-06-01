a585d32e6d42d34ce1642f1c01f51fc5cbd221e8
RenzoJse <renzojose007@gmail.com>
2025-06-01 20:16:51 -0300

    Code: KeyStrategy Code

---
+++ src/ObjectSim.Security/Strategy/KeyStrat/KeyStrategy.cs
@@ -0,0 +1,17 @@
+﻿using ObjectSim.DataAccess.Interface;
+
+namespace ObjectSim.Security.Strategy.KeyStrat;
+
+public class KeyStrategy(IRepository<Key> keyRepository) : IValidationStrategy
+{
+
+    public bool WhichStrategy(string key)
+    {
+        return Guid.TryParse(key, out _);
+    }
+
+    public bool Validate(string key)
+    {
+        return keyRepository.Exists(k => k.AccessKey.ToString() == key);
+    }
+}
