a8cd9bd51ac31af463b21e94b527f0ccfea2ae64
RenzoJse <renzojose007@gmail.com>
2025-06-02 11:52:30 -0300

    Code: Created MethodExecution module

---
+++ ObjectSimFrontEnd/src/app/method-execution/method-execution.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { MethodExecutionRoutingModule } from './method-execution-routing.module';
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MethodExecutionRoutingModule,
+    ]
+})
+export class MethodExecutionModule { }
