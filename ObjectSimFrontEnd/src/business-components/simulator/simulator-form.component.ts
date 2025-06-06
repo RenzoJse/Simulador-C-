3cf4dfac3488fa6ae9eebaf8e20f3795500b34f9
RenzoJse <renzojose007@gmail.com>
2025-06-06 13:57:13 -0300

    Code: Added dropdown components

--- ObjectSimFrontEnd/src/business-components/simulator/simulator-form.component.ts
+++ ObjectSimFrontEnd/src/business-components/simulator/simulator-form.component.ts
@@ -5,13 +5,17 @@ import { NgIf } from '@angular/common';
 import { FormInputComponent } from '../../components/form/form-input/form-input.component';
 import { FormButtonComponent } from '../../components/form/form-button/form-button.component';
 import { FormComponent } from '../../components/form/form/form.component';
+import { ClassDropdownComponent } from '../../business-components/class/dropdown/class-dropdown.component';
+import { MethodDropdownComponent } from '../../business-components/method/dropdown/method-dropdown.component';
 
 import CreateSimulatedExecutionModel from '../../backend/services/simulator/models/create-simulated-execution.model';
 
 @Component({
     selector: 'app-simulator-form',
     standalone: true,
-    imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent, NgIf, FormComponent],
+    imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent,
+        NgIf, FormComponent, ClassDropdownComponent,
+        MethodDropdownComponent],
     templateUrl: './simulator-form.component.html'
 })
 
