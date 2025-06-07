fb415f8404772f84d75447566a50ef31e14f899b
Renzo Jose <renzojose007@gmail.com>
2025-06-07 19:37:46 -0300

    Code: Created auth guards

---
+++ ObjectSimFrontEnd/src/guard/authentication.guard.ts
@@ -0,0 +1,14 @@
+﻿import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authenticationGuard: CanActivateFn = (route, state) => {
+    const isNotAuthenticated = !localStorage.getItem('key') === null;
+    
+    if(isNotAuthenticated){
+        const router = inject(Router);
+        
+        return router.parseUrl('/insterKey');
+    }
+    
+    return true;
+};
---
+++ ObjectSimFrontEnd/src/guard/not-authenticated.guard.ts
@@ -0,0 +1,13 @@
+﻿import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const notAuthenticatedGuard: CanActivateFn = (route, state) => {
+    const isAuthenticated = localStorage.getItem('key') !== null;
+
+    if (isAuthenticated) {
+        const router = inject(Router);
+        return router.parseUrl('/home');
+    }
+
+    return true;
+}
