fac605c2561dae9bc15cf5079c6584e2502a1ece
Renzo Jose <renzojose007@gmail.com>
2025-06-08 16:59:13 -0300

    Fix: Made change in front services, now it sends local key

--- ObjectSimFrontEnd/src/backend/services/simulator/simulator.service.ts
+++ ObjectSimFrontEnd/src/backend/services/simulator/simulator.service.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import CreateSimulatedExecutionModel from './models/create-simulated-execution.model';
@@ -10,10 +10,20 @@ import CreateSimulatedExecutionModel from './models/create-simulated-execution.m
 export class SimulatorService {
     private readonly apiUrl = 'http://localhost:5018/api/simulator';
 
+    protected get headers(){
+        return {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: localStorage.getItem('key') ?? '',
+            })
+        };
+    }
+    
     constructor(private http: HttpClient) {}
 
     simulateExecution(simulateExecution: CreateSimulatedExecutionModel): Observable<any> {
-        return this.http.post(this.apiUrl, simulateExecution);
+        return this.http.post(this.apiUrl, simulateExecution, this.headers);
     }
 
 }
