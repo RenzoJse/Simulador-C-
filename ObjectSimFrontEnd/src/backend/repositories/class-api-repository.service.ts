ac4cea1a38fdb2ed0dcc19f36f31ac35c0de5cdb
Renzo Jose <renzojose007@gmail.com>
2025-06-08 18:49:32 -0300

    Code: Added front enviroments

---
+++ ObjectSimFrontEnd/src/backend/repositories/class-api-repository.service.ts
@@ -0,0 +1,13 @@
+﻿
+@Injectable({
+    providedIn: 'root',
+})
+export class ClassApiRepository extends ApiRepository {
+
+    constructor(http: HttpClient){
+        super(a.objectsim, 'api/class', http);
+    }
+    s
+    getClassById(classId: string): Observable<Class> {
+        return this.get<Class>(classId);
+        
