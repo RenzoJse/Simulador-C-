53857b83639742ac423b5e747ad4c24a7eea0b13
Renzo Jose <renzojose007@gmail.com>
2025-06-08 20:05:40 -0300

    Code: Created attribute api repository

---
+++ ObjectSimFrontEnd/src/backend/repositories/attribute-api-repository.service.ts
@@ -0,0 +1,43 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import enviroments from '../../environments/index';
+import ApiRepository from './api-repository';
+import AttributeDtoModel from '../../backend/services/attribute/model/attribute-dto.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AttributeApiRepository extends ApiRepository {
+    
+    constructor(http: HttpClient) {
+        super(enviroments.objectsim, 'api/attributes', http);
+    }
+    
+    createAttribute(attribute: AttributeDtoModel): Observable<AttributeDtoModel> {
+        return this.post<AttributeDtoModel>(attribute);
+    }
+    
+    getAllAttributes(): Observable<any>{
+        //TODO
+        return this.get<any>();
+    }
+    
+    deleteAttribute(id: string): Observable<any> {
+        const url = `/${id}`;
+        return this.delete(url);
+    }
+    
+    getAttributeById(attributeId: string): Observable<AttributeDtoModel> {
+        return this.get<AttributeDtoModel>(attributeId);
+    }
+    
+    getAttributesByClassId(classId: string): Observable<AttributeDtoModel[]> {
+        return this.get<AttributeDtoModel[]>(`by-class/${classId}`);
+    }
+    
+    updateAttribute(id: string, attribute: AttributeDtoModel): Observable<AttributeDtoModel> {
+        return this.patch<AttributeDtoModel>(`${id}`, attribute);
+    }
+}
