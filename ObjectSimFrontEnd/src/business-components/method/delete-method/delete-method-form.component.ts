e2d63f070a0c0795ecf4fdcde22608d0f83d09e8
RenzoJse <renzojose007@gmail.com>
2025-06-12 11:36:42 -0300

    Fix: Updated delete method component

--- ObjectSimFrontEnd/src/business-components/method/delete-method/delete-method-form.component.ts
+++ ObjectSimFrontEnd/src/business-components/method/delete-method/delete-method-form.component.ts
@@ -1,32 +1,59 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CommonModule, NgIf }      from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+import { FormInputComponent } from '../../../components/form/form-input/form-input.component';
+import { FormButtonComponent } from '../../../components/form/form-button/form-button.component';
+import { FormComponent } from '../../../components/form/form/form.component';
+import { MethodDropdownComponent } from '../dropdown/method-dropdown.component';
 
 @Component({
   selector: 'app-delete-method-form',
   standalone: true,
-  imports: [ CommonModule, ReactiveFormsModule, NgIf ],
+  imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent,
+    NgIf, FormComponent, MethodDropdownComponent],
   templateUrl: './delete-method-form.component.html'
 })
+
 export class DeleteMethodFormComponent {
-  @Input() methods: { id: string; name: string }[] = [];
-  @Input() loading = false;
-  @Input() error: string | null = null;
-  @Output() atDelete = new EventEmitter<string>();
+  @Input() title: string = '';
+  @Output() atDelete = new EventEmitter<string | undefined>();
 
-  form: FormGroup;
+  deleteMethodForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      methodId: ['', Validators.required]
+  deleteMethodFormStatus: {
+    loading?: true;
+    error?: string;
+  } | null = null;
+
+  MethodId: string | undefined;
+
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
+    this.deleteMethodForm = this.fb.group({
+      methodId: ['']
     });
   }
 
-  onSubmit() {
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
-      return;
+  public onSubmit() {
+    if (this.deleteMethodForm.valid) {
+      const {methodId} = this.deleteMethodForm.value;
+      this.atDelete.emit(this.deleteMethodForm.value.methodId);
+    } else {
+      this.markAsTouched();
+      console.log('Invalid form:', this.deleteMethodForm.errors);
     }
-    this.atDelete.emit(this.form.value.methodId);
   }
+
+  private markAsTouched() {
+    Object.values(this.deleteMethodForm.controls).forEach(control => {
+      control.markAsTouched();
+    });
+  }
+
+  updateSelectedMethodId(event: { methodId: string | undefined; }) {
+    this.MethodId = event.methodId;
+    this.deleteMethodForm.patchValue({ methodId: event.methodId });
+    this.cdr.detectChanges();
+  }
+
 }
