f98e2b73efbf464fc5a4328ab6edbbf9becf1fdf
RenzoJse <renzojose007@gmail.com>
2025-06-13 13:54:36 -0300

    Code: Created methodListing in app

---
+++ ObjectSimFrontEnd/src/app/method/method-list/method-listing.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component } from '@angular/core';
+import MethodDTO from "../../../backend/services/method/models/method-dto.model";
+@Component({
+    selector: 'app-method-listing',
+    templateUrl: './method-listing.component.html',
+})
+
+export class MethodListingComponent {
+
+    selectedMethod: string | null = null;
+
+    onSelectedMethod(method: MethodDTO | undefined): void {
+        if (method) {
+            this.selectedMethod = method.name;
+        } else {
+            this.selectedMethod = null;
+        }
+    }
+}
+
+
