7cdf8c16e0f5711ece278a2bebf4b783f12c0b0e
RenzoJse <renzojose007@gmail.com>
2025-06-14 14:31:15 -0300

    Code: Created output api repository front

---
+++ ObjectSimFrontEnd/src/backend/repositories/outputModel-api-repository.service.ts
@@ -0,0 +1,27 @@
+﻿import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import enviroments from '../../environments/index';
+import ApiRepository from './api-repository';
+import CreateSimulatedExecutionModel from '../../backend/services/simulator/models/create-simulated-execution.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class OutputModelApiRepositoryService extends ApiRepository {
+
+    constructor(http: HttpClient) {
+        super(enviroments.objectsim, 'api/outputModel', http);
+    }
+
+    uploadDllFile(file: File): Observable<any> {
+        return this.post<any>(file);
+    }
+
+    getImplementationList(): Observable<any> {
+        return this.get<any>();
+    }
+
+}
+
