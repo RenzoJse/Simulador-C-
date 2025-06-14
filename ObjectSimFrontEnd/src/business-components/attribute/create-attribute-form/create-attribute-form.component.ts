1bc397857cb46116d44e2ee96aec9815b8820e72
joaquincalvo <joaquincalvo7@gmail.com>
2025-06-14 01:02:17 -0300

    code:dropdowns

--- ObjectSimFrontEnd/src/business-components/attribute/create-attribute-form/create-attribute-form.component.ts
+++ ObjectSimFrontEnd/src/business-components/attribute/create-attribute-form/create-attribute-form.component.ts
@@ -1,86 +1,89 @@
 ﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule, NgIf } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
 
 import { FormInputComponent } from '../../../components/form/form-input/form-input.component';
 import { FormButtonComponent } from '../../../components/form/form-button/form-button.component';
 import { FormComponent } from '../../../components/form/form/form.component';
-
+import { ClassDropdownComponent } from '../../class/dropdown/class-dropdown.component';
+import { DataTypeDropdownComponent } from '../../datatype/datatype-dropdown/data-type-dropdown.component';
 import CreateAttributeModel from '../../../backend/services/attribute/models/create-attribute.model';
 
 @Component({
-    selector: 'app-create-attribute-form',
-    standalone: true,
-    imports: [ReactiveFormsModule, FormInputComponent, FormButtonComponent, NgIf, FormComponent,CommonModule],
-    templateUrl: './create-attribute-form.component.html'
+  selector: 'app-create-attribute-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    NgIf,
+    FormInputComponent,
+    FormButtonComponent,
+    FormComponent,
+    ClassDropdownComponent,
+    DataTypeDropdownComponent
+  ],
+  templateUrl: './create-attribute-form.component.html'
 })
-
 export class CreateAttributeFormComponent {
-    @Input() tittle : string = '';
-    @Input() availableClasses: { value: string; tag: string }[] = [];
-    @Input() availableDataTypes: { value: string; tag: string }[] = [];
-    createAttributeForm: FormGroup;
-    @Output() atSubmit = new EventEmitter<CreateAttributeModel>();
+  @Input() title: string = '';
+  @Output() atSubmit = new EventEmitter<CreateAttributeModel>();
 
-    createAttributeStatus: {
-        loading?: true;
-        error?: string;
-    } | null = null;
+  createAttributeForm: FormGroup;
+  selectedClassId: string = '';
+  selectedDataTypeId: string = '';
 
-    Visibilities: { value: string; tag: string }[] = [
-        { value: 'Public', tag: 'Public' },
-        { value: 'Private', tag: 'Private' },
-        { value: 'Protected', tag: 'Protected' }
-    ];
+  createAttributeStatus: {
+    loading?: true;
+    error?: string;
+  } | null = null;
 
-    Static: { value: string; tag: string }[] = [
-        { value: "true", tag: 'Static' },
-        { value: "false", tag: 'Non-Static' }
-    ];
+  Visibilities = [
+    { value: 'Public', tag: 'Public' },
+    { value: 'Private', tag: 'Private' },
+    { value: 'Protected', tag: 'Protected' }
+  ];
 
-    private classId: string = '';
+  Static = [
+    { value: 'true', tag: 'Static' },
+    { value: 'false', tag: 'Non-Static' }
+  ];
 
-    constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) {
-        this.createAttributeForm = this.formBuilder.group({
-            name: ['', [Validators.required,
-                Validators.maxLength(10),
-                Validators.minLength(1)]],
-            dataTypeID: ['', Validators.required],
-            visibility: ['', Validators.required],
-            isStatic: [false, Validators.required],
-            classId: ['', Validators.required],
-        });
+  constructor(private formBuilder: FormBuilder) {
+    this.createAttributeForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(10)]],
+      visibility: ['', Validators.required],
+      isStatic: ['false', Validators.required]
+    });
+  }
 
-        this.route.paramMap.subscribe(params => {
-            this.classId = params.get('classId') ?? '';
-        });
+  onSubmit() {
+    if (this.createAttributeForm.invalid || !this.selectedClassId || !this.selectedDataTypeId) {
+      this.markAsTouched();
+      return;
     }
 
-    public onSubmit() {
-        console.log('Form attribute submitted:', this.createAttributeForm.value);
+    const formValue = this.createAttributeForm.value;
 
-        if(this.createAttributeForm.invalid) {
-            this.markAsTouched()
-            return;
-        }
+    const newAttribute: CreateAttributeModel = {
+      name: formValue.name,
+      dataTypeId: this.selectedDataTypeId,
+      visibility: formValue.visibility,
+      classId: this.selectedClassId,
+      isStatic: formValue.isStatic === 'true'
+    };
 
-        var formValue = this.createAttributeForm.value;
-        var newAttribute: CreateAttributeModel = {
-            name: formValue.name,
-            visibility: formValue.visibility,
-            dataTypeId: formValue.dataTypeID,
-            classId: this.classId,
-            isStatic: formValue.isStatic === "true"
-        };
+    this.atSubmit.emit(newAttribute);
+  }
 
-        this.atSubmit.emit(newAttribute);
-        console.log('try to create new attribute: ', newAttribute);
-    }
+  onClassSelected(event: { classId: string | undefined }) {
+    this.selectedClassId = event.classId ?? '';
+  }
 
-    private markAsTouched() {
-        Object.values(this.createAttributeForm.controls).forEach(control => {
-            control.markAsTouched();
-        });
-    }
+  onDataTypeSelected(dataTypeId: string) {
+    this.selectedDataTypeId = dataTypeId;
+  }
+
+  private markAsTouched() {
+    Object.values(this.createAttributeForm.controls).forEach(control => control.markAsTouched());
+  }
 }
