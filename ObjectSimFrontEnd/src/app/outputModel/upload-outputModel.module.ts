2b5360dd9a13fe5c2aba561f6a090ee41362b6f2
RenzoJse <renzojose007@gmail.com>
2025-06-14 16:16:58 -0300

    Code: Implemented outputModel in app front

---
+++ ObjectSimFrontEnd/src/app/outputModel/upload-outputModel.module.ts
@@ -0,0 +1,21 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { ButtonComponent } from '../../components/button/button.component';
+
+import { UploadOutputModelRoutingModule } from './upload-outputModel-routing.module';
+import { UploadOutputModelComponent } from "./upload-outputModel/upload-outputModel.component";
+
+@NgModule({
+    declarations: [
+        UploadOutputModelComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        UploadOutputModelRoutingModule,
+        ButtonComponent
+    ]
+})
+export class UploadOutputModelModule { }
---
+++ ObjectSimFrontEnd/src/app/outputModel/upload-outputModel/upload-outputModel.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component } from '@angular/core';
+
+import { OutputModelService } from "../../../backend/services/outputModel/outputModel.service";
+
+@Component({
+    selector: 'app-upload-outputModel',
+    templateUrl: 'upload-outputModel.component.html'
+})
+
+export class UploadOutputModelComponent {
+    constructor(private readonly _outputModelService: OutputModelService) {
+    }
+
+    protected atSubmit(uploadedModel: { file: File }) {
+        if (!uploadedModel.file) {
+            console.error('No file selected for upload.');
+            return;
+        }
+
+        this._outputModelService.uploadDllFile(uploadedModel).subscribe({
+            next: (response) => {
+                console.log('Upload successful:', response);
+            },
+            error: (error) => {
+                console.error('Error uploading output model:', error);
+            }
+        });
+    }
+}
