d5bf53f9753c221389cd6edce302b1e901aab57c
RenzoJse <renzojose007@gmail.com>
2025-06-14 14:33:21 -0300

    Code: Implemented OutputModelService front

---
+++ ObjectSimFrontEnd/src/backend/services/outputModel/outputModel.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { OutputModelApiRepositoryService } from '../../repositories/outputModel-api-repository.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class OutputModelService {
+    constructor(private readonly _outputModelRepository: OutputModelApiRepositoryService) {}
+
+    public uploadDllFile(file: File): Observable<any> {
+        return this._outputModelRepository.uploadDllFile(file);
+    }
+
+    public getImplementationList(): Observable<any> {
+        return this._outputModelRepository.getImplementationList();
+    }
+
+}
