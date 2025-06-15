25e0a6cde0781b72b9995e9b2ee4f91b6eec61ea
RenzoJse <renzojose007@gmail.com>
2025-06-15 16:41:04 -0300

    Code: Created add invoke component

---
+++ ObjectSimFrontEnd/src/app/method/add-invoke-method/add-invoke-method.component.ts
@@ -0,0 +1,38 @@
+﻿import { Component, Inject } from '@angular/core';
+
+import { MethodService } from "../../../backend/services/method/method.service";
+import AddInvokeMethodModel from "../../../backend/services/method/models/add-invoke-method.model";
+
+@Component({
+    selector: 'app-add-invoke-method',
+    templateUrl: './add-invoke-method.component.html',
+})
+
+export class AddInvokeMethodComponent {
+
+    status: { loading?: true; error?: string } | null = null;
+
+    constructor(
+        @Inject(MethodService) private readonly _methodService : MethodService,
+    ) {
+    }
+
+    protected atSubmit(methodToAdd: AddInvokeMethodModel) {
+        this.status = { loading: true };
+
+        this._methodService.addInvokeMethods(methodToAdd).subscribe({
+            next: () => {
+
+
+            },
+            error: (error:any) => {
+                if (error.status === 400 && error.Message) {
+                    this.status = { error: error.Message };
+                } else {
+                    this.status = { error: error.message || 'Error adding invoke method.' };
+                }
+            },
+        });
+    }
+
+}
