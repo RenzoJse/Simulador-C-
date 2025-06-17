971f88cfd285ccf7d0e27359216688fd1551f64d
Matias Fontes <matifontes13@gmail.com>
2025-06-17 12:57:33 -0300

    refactor: output model front

--- tests/ObjectSim.WebApi.Test/Controllers/OutputModelControllerTest.cs
+++ tests/ObjectSim.WebApi.Test/Controllers/OutputModelControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using ObjectSim.IBusinessLogic;
@@ -35,6 +36,34 @@ public class OutputModelControllerTest
         Assert.IsInstanceOfType(result, typeof(OkObjectResult));
     }
 
+    [TestMethod]
+    public void UploadDll_ServiceThrowsException_ShouldPropagateException()
+    {
+        var fileMock = new Mock<IFormFile>();
+        fileMock.Setup(f => f.FileName).Returns("bad.dll");
+        fileMock.Setup(f => f.OpenReadStream()).Returns(new MemoryStream());
+        _outputModelTransformerService
+            .Setup(s => s.UploadDll(It.IsAny<Stream>(), "bad.dll"))
+            .Throws(new InvalidOperationException("upload fail"));
+
+        Action act = () => _controllerTest.UploadDll(fileMock.Object);
+        act.Should().Throw<InvalidOperationException>()
+           .WithMessage("upload fail");
+    }
+
+    [TestMethod]
+    public void UploadDll_ValidDllFile_ReturnsSuccessMessage()
+    {
+        var fileMock = new Mock<IFormFile>();
+        fileMock.Setup(f => f.FileName).Returns("test.dll");
+        fileMock.Setup(f => f.OpenReadStream()).Returns(new MemoryStream());
+
+        var result = _controllerTest.UploadDll(fileMock.Object) as OkObjectResult;
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(200, result.StatusCode);
+        Assert.AreEqual("DLL uploaded successfully.", result.Value);
+    }
     #endregion
 
     #region Get
@@ -60,5 +89,46 @@ public class OutputModelControllerTest
         _outputModelTransformerService.Verify(s => s.GetImplementationList(), Times.Once);
     }
 
+    [TestMethod]
+    public void GetImplementationList_WhenNone_ReturnsEmptyDtoList()
+    {
+        _outputModelTransformerService
+            .Setup(s => s.GetImplementationList())
+            .Returns(new List<string>());
+
+        var ok = _controllerTest.GetImplementationList() as OkObjectResult;
+
+        Assert.IsNotNull(ok);
+        var list = ok.Value as List<OutputModelNameDtoOut>;
+        Assert.IsNotNull(list);
+        Assert.AreEqual(0, list.Count);
+    }
+
+    [TestMethod]
+    public void GetImplementationList_ServiceThrowsException_ShouldPropagateException()
+    {
+        _outputModelTransformerService
+            .Setup(s => s.GetImplementationList())
+            .Throws(new Exception("fail get list"));
+
+        Action act = () => _controllerTest.GetImplementationList();
+        act.Should().Throw<Exception>()
+           .WithMessage("fail get list");
+    }
+
+    [TestMethod]
+    public void GetImplementationList_ShouldMapEachNameToDto()
+    {
+        var impls = new List<string> { "A", "B", "C" };
+        _outputModelTransformerService
+            .Setup(s => s.GetImplementationList())
+            .Returns(impls);
+
+        var ok = _controllerTest.GetImplementationList() as OkObjectResult;
+        var dtos = ok!.Value as List<OutputModelNameDtoOut>;
+
+        Assert.AreEqual(3, dtos!.Count);
+        CollectionAssert.AreEqual(impls, dtos.Select(d => d.Name).ToList());
+    }
     #endregion
 }
